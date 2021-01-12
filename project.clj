4f1867ce88a7f18aaa0334471a91c8372b47a6bc
Wanderson Ferreira <iagwanderson@gmail.com>
2021-01-12 20:59:45 -0300

    config files using config/yogthos and component

--- project.clj
+++ project.clj
@@ -2,7 +2,12 @@
   :description "Framework"
   :url "https://github.com/Flexiana/framework"
   :license {:name "FIXME" :url "FIXME"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.stuartsierra/component "1.0.0"]
+                 [yogthos/config "1.1.7"]]
   :target "target/%s/"
+  :profiles {:dev   {:resource-paths ["config/dev"]}
+             :local {:resource-paths ["config/local"]}
+             :prod  {:resource-paths ["config/prod"]}}
   :main framework.components.core
   )
