0dd5a8534c1b5ca12dfe9b7f7c2edc991d4e4233
Wanderson Ferreira <iagwanderson@gmail.com>
2021-01-12 08:31:07 -0300

    clean project.clj boilerplate

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject framework "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject framework "0.1.0"
+  :description "Framework"
+  :url "https://github.com/Flexiana/framework"
+  :license {:name "FIXME" :url "FIXME"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :target "target/%s/"
   :main framework.components.core
