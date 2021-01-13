c3da472899703766b6a7f9823433f0d6de9ceba6
Wanderson Ferreira <iagwanderson@gmail.com>
2021-01-13 16:06:44 -0300

    initial commit on adding database functionality into framework

--- src/framework/db/storage.clj
+++ src/framework/db/storage.clj
@@ -1,12 +1,16 @@
 (ns framework.db.storage
-  (:require [com.stuartsierra.component :as component]))
+  (:require [com.stuartsierra.component :as component]
+            [next.jdbc :as jdbc]))
 
 (defrecord PostgreSQL [config]
   component/Lifecycle
   (start [this]
-    (println "Start Postgresql " config))
+    (let [datasource (jdbc/get-datasource config)]
+     (-> this
+         (assoc :datasource datasource)
+         (assoc :connection (jdbc/get-connection datasource)))))
   (stop [this]
-    (println "Stop Postgresql")))
+    (dissoc this :datasource :connection)))
 
 (defn postgresql
   [config]
