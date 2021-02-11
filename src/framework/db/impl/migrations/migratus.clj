fe5833f6006a10afd1e845f905075ad518b2b80e
Bohdan Korniyenko <me@yatagan.xyz>
2021-02-11 14:52:43 +0200

    Fix migrations path

--- src/framework/db/impl/migrations/migratus.clj
+++ src/framework/db/impl/migrations/migratus.clj
@@ -13,10 +13,17 @@
 
 (defn create
   [config name]
-  (let [migration-dir (migrations/find-or-create-migration-dir
-                        (utils/get-parent-migration-dir config)
-                        (utils/get-migration-dir config))
+  (let [migration-dir  (migrations/find-or-create-migration-dir
+                         (utils/get-parent-migration-dir config)
+                         (utils/get-migration-dir config))
         migration-name (migrations/->kebab-case (str (timestamp) name))]
     (doseq [mig-file (proto/migration-files* :edn migration-name)]
       (.createNewFile (io/file migration-dir mig-file)))
     [migration-name migration-dir]))
+
+(defn get-migrations-folder-path
+  [config]
+  (-> (migrations/find-or-create-migration-dir
+        (utils/get-parent-migration-dir config)
+        (utils/get-migration-dir config))
+      (.getAbsolutePath)))
