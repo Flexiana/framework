be75221e36d091357ab5014e2d3930f0d21289b5
Ian Fernandez <d.ian.b@live.com>
2021-02-11 14:49:14 -0300

    cljstyle

--- src/framework/app/view/core.clj
+++ src/framework/app/view/core.clj
@@ -1,8 +1,8 @@
 (ns framework.app.view.core
-  (:require [xiana.core :as xiana]
-            [tongue.core :as tongue]
-            [hiccup.core :as hiccup]
-            ))
+  (:require
+    [hiccup.core :as hiccup]
+    [tongue.core :as tongue]
+    [xiana.core :as xiana]))
 
 (defn ->html
   [hic]
@@ -11,18 +11,18 @@
 (defn set-dictionary
   [state dict-map]
   (xiana/ok
-   (assoc state :dict-fn (tongue/build-translate dict-map))))
+    (assoc state :dict-fn (tongue/build-translate dict-map))))
 
 (defn set-response
   [state response]
   (xiana/ok
-   (assoc state :response response)))
+    (assoc state :response response)))
 
 (defmulti render-view
   (fn [action] action))
 
 (defmethod render-view :is-html
-  [{:keys [layout template lang dict-fn]} state]
+  [{:keys [layout template lang dict-fn]} _state]
   (comp layout (template dict-fn lang)))
 
 (defmethod render-view :is-api
@@ -34,5 +34,5 @@
   [state]
   (let [{:keys [is-api is-html]} state]
     (cond->
-        (is-html) (render-view :is-html state)
-        (is-api) (render-view :is-api state))))
+      (is-html) (render-view :is-html state)
+      (is-api) (render-view :is-api state))))
--- src/framework/app/view/template.clj
+++ src/framework/app/view/template.clj
@@ -1,11 +1,11 @@
 (ns framework.app.view.template
-  (:require [xiana.core :as xiana]))
-
+  (:require
+    [xiana.core :as xiana]))
 
 (defn set-layout
   [state layout]
   (xiana/ok
-   (assoc state :layout layout)))
+    (assoc state :layout layout)))
 
 (defn get-layout
   [controller]
@@ -17,7 +17,7 @@
   "Setting the hiccup template function to state map."
   [state template]
   (xiana/ok
-   (assoc state :template template)))
+    (assoc state :template template)))
 
 (defn get-template
   "Return the hiccup function."
@@ -31,4 +31,4 @@
   [state]
   (let [{{:keys [headers]} :http-request} state]
     (xiana/ok
-     (assoc state :lang (get headers "accept-language")))))
+      (assoc state :lang (get headers "accept-language")))))
