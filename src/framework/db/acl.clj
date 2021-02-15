1cfd54673c6a13390b20260317f711f83b44bc5b
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-02-15 18:20:22 +0100

    WIP

--- src/framework/db/acl.clj
+++ src/framework/db/acl.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :as str]))
 
 
-(defn action [query]
+(defn action-table [query]
   (let [q (str/replace query ";" " ")
         r (map
             (fn [[_ action _ on]] [(keyword (str/lower-case action)) on])
@@ -18,3 +18,19 @@
 (defn user-id [env]
   (:id (fetch (:session env) :user)))
 
+(defn acl [{:keys [user-roles]} query]
+  (let [action (action-table query)]
+    (println "action: " action "roles" (flatten (vals user-roles)))
+    (filter (set action) user-roles)))
+
+
+(action-table "SELECT * FROM items;")
+
+(def action '([:select "items"] [:delete "items"]))
+(def roles [{:select "items", :filter :all} {:select "users", :filter :own} {:update "users", :filter :own} {:delete "users", :filter :own} {:insert "addresses", :filter :own} {:update "addresses", :filter :own} {:select "addresses", :filter :own} {:delete "addresses", :filter :own} {:select "carts", :filter :own} {:insert "carts", :filter :own} {:update "carts", :filter :own} {:delete "carts", :filter :own}])
+
+(filter :select roles)
+
+
+(map #(dissoc % :filter) roles)
+(map first action)
--- test/framework/db/acl_test.clj
+++ test/framework/db/acl_test.clj
@@ -1,26 +1,26 @@
 (ns framework.db.acl-test
   (:require [clojure.test :refer :all])
-  (:require [framework.db.acl :refer [action]]))
+  (:require [framework.db.acl :refer [action-table user-id acl]]))
 
 
 
 (deftest action-test
-  (is (= '([:select "test_table"]) (action "SELECT * FROM test_table")))
-  (is (= '([:insert "films"]) (action "INSERT INTO films (code, title, did, date_prod, kind)\n    VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');"))) (is (= '([:insert "films"]) (action "INSERT INTO films (code, title, did, date_prod, kind)\n    VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');")))
-  (is (= '([:delete "films"] [:select "producers"]) (action "DELETE FROM films\n  WHERE producer_id IN (SELECT id FROM producers WHERE name = 'foo');"))) (is (= '([:insert "films"]) (action "INSERT INTO films (code, title, did, date_prod, kind)\n    VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');")))
-  (is (= '([:update "films"]) (action "UPDATE films SET kind = 'Dramatic' WHERE kind = 'Drama';"))) (is (= '([:insert "films"]) (action "INSERT INTO films (code, title, did, date_prod, kind)\n    VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');")))
-  (is (= '([:drop "conversation"]) (action "DROP TABLE conversation;")))
-  (is (= '([:insert "films"]) (action "INSERT INTO films (code, title, did, date_prod, kind)\n    VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');")))
-  (is (= '([:truncate "bigtable"]) (action "TRUNCATE bigtable;")))
-  (is (= '([:alter "employees"]) (action "ALTER TABLE employees ADD COLUMN address text")))
-  (is (= '([:alter "employees"]) (action "ALTER TABLE employees DROP COLUMN address"))))
+  (is (= '([:select "test_table"]) (action-table "SELECT * FROM test_table")))
+  (is (= '([:insert "films"]) (action-table "INSERT INTO films (code, title, did, date_prod, kind)\n    VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');"))) (is (= '([:insert "films"]) (action-table "INSERT INTO films (code, title, did, date_prod, kind)\n    VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');")))
+  (is (= '([:delete "films"] [:select "producers"]) (action-table "DELETE FROM films\n  WHERE producer_id IN (SELECT id FROM producers WHERE name = 'foo');"))) (is (= '([:insert "films"]) (action-table "INSERT INTO films (code, title, did, date_prod, kind)\n    VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');")))
+  (is (= '([:update "films"]) (action-table "UPDATE films SET kind = 'Dramatic' WHERE kind = 'Drama';"))) (is (= '([:insert "films"]) (action-table "INSERT INTO films (code, title, did, date_prod, kind)\n    VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');")))
+  (is (= '([:drop "conversation"]) (action-table "DROP TABLE conversation;")))
+  (is (= '([:insert "films"]) (action-table "INSERT INTO films (code, title, did, date_prod, kind)\n    VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');")))
+  (is (= '([:truncate "bigtable"]) (action-table "TRUNCATE bigtable;")))
+  (is (= '([:alter "employees"]) (action-table "ALTER TABLE employees ADD COLUMN address text")))
+  (is (= '([:alter "employees"]) (action-table "ALTER TABLE employees DROP COLUMN address"))))
 
 (def mock-db
-  {:users             [{:id              1
-                        :name            "John"
-                        :surname         "Doe"
-                        :email           "doe.john@test.com"
-                        :role            :customer}
+  {:users             [{:id      1
+                        :name    "John"
+                        :surname "Doe"
+                        :email   "doe.john@test.com"
+                        :role    :customer}
                        {:id      2
                         :name    "Admin"
                         :surname "Doe"
@@ -41,27 +41,27 @@
                         :surname "Service"
                         :email   "postoffice@warehouse.com"
                         :role    :postal-worker}
-                       {:id              6
-                        :name            "Janet"
-                        :surname         "Doe"
-                        :email           "doe.janet@test.com"
-                        :role            :customer}]
-   :items [{:id 1
-            :name "salami slicer"
-            :price {:value 28.49
-                    :currency "USD"}}
-           {:id 2
-            :name "bread knife"
-            :price {:value 12.97
-                    :currency "USD"}}
-           {:id 3
-            :name "Meat tenderizer"
-            :price {:value 14.99
-                    :currency "USD"}}
-           {:id 4
-            :name "cheese grater"
-            :price {:value 12.49
-                    :currency "USD"}}]
+                       {:id      6
+                        :name    "Janet"
+                        :surname "Doe"
+                        :email   "doe.janet@test.com"
+                        :role    :customer}]
+   :items             [{:id    1
+                        :name  "salami slicer"
+                        :price {:value    28.49
+                                :currency "USD"}}
+                       {:id    2
+                        :name  "bread knife"
+                        :price {:value    12.97
+                                :currency "USD"}}
+                       {:id    3
+                        :name  "Meat tenderizer"
+                        :price {:value    14.99
+                                :currency "USD"}}
+                       {:id    4
+                        :name  "cheese grater"
+                        :price {:value    12.49
+                                :currency "USD"}}]
    :addresses         [{:id          1
                         :country     "Czech Republic"
                         :city        "Prague"
@@ -111,16 +111,31 @@
                                            {:update "carts"
                                             :filter :all}
                                            {:select "carts"
-                                            :filter :all}]}
-                       {:postal-worker [{:select "carts"
-                                         :filter :all}
-                                        {:update "carts"
-                                         :filter :all}
-                                        {:select :users
-                                         :filter :all}
-                                        {:select "addresses"
-                                         :filter :all}]}
-                       {:shop-worker [{:all    "items"
-                                       :filter :all}]}
-                       {:administrator [{:all    :all
-                                         :filter :all}]}]})
+                                            :filter :all}]
+                        :postal-worker    [{:select "carts"
+                                            :filter :all}
+                                           {:update "carts"
+                                            :filter :all}
+                                           {:select :users
+                                            :filter :all}
+                                           {:select "addresses"
+                                            :filter :all}]
+                        :shop-worker      [{:all    "items"
+                                            :filter :all}]
+                        :administrator    [{:all    :all
+                                            :filter :all}]}]})
+
+(defn fetch-db [db table where]
+  (if-let [found (filter where (get db table))]
+    (if (next found) found (first found))))
+
+(defn env [env user-id]
+  (let [user (fetch-db mock-db :users #(= user-id (:id %)))
+        role (:role user)]
+    (println role)
+    (cond-> env
+            user (assoc-in [:session :user] user)
+            role (assoc :user-roles (fetch-db mock-db :roles role)))))
+(env {} 1)
+
+(acl (env {} 1) "SELECT * FROM items;")
