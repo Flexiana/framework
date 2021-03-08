f69dc7988c4d0fd67a2e1f966fa8eaddaefcb037
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-03-08 17:36:03 +0100

    Adapt into xiana Flow->

--- src/framework/components/acl/core.clj
+++ src/framework/components/acl/core.clj
@@ -1,19 +1,21 @@
-(ns framework.components.acl.core)
+(ns framework.components.acl.core
+  (:require
+    [xiana.core :as xiana]))
 
-(defn isAllowed
-  "Examine if the user is able to access to a resource with the provided action.
-  If it's able to, returns anything what is provided in 'permissions' corresponding :filter field.
-  If it's isn't then returns \"false\"
+(defn has-access
+  "Examine if the user is has access to a resource with the provided action.
+  If it has, returns anything what is provided in 'permissions' corresponding :filter field.
+  If isn't then returns \"false\"
   'permissions' is a map keyed by name of permissions.
   'user' is optional, but if it missing you must provide the 'role' field in action.
   'access' defines the role, resource and privilege what needs to be achieved.
   If user is provided, the role will be resolved from it."
   ([permissions user access]
    (cond
-     (not (:is_active user)) (isAllowed permissions (assoc access :role :guest))
-     (:is_superuser user) (isAllowed permissions (assoc access :role :superuser))
-     (:is_staff user) (isAllowed permissions (assoc access :role :staff))
-     (:is_active user) (isAllowed permissions (assoc access :role :member))))
+     (not (:is_active user)) (has-access permissions (assoc access :role :guest))
+     (:is_superuser user) (has-access permissions (assoc access :role :superuser))
+     (:is_staff user) (has-access permissions (assoc access :role :staff))
+     (:is_active user) (has-access permissions (assoc access :role :member))))
   ([permissions {:keys [role resource privilege]}]
    (let [granted (->> (get permissions role)
                       (filter #(#{resource :all} (:resource %)))
@@ -22,3 +24,43 @@
        (:filter granted)
        false))))
 
+(def action-mapping
+  {:get    :read
+   :post   :update
+   :put    :create
+   :delete :delete})
+
+(defn ->resource
+  [uri]
+  (re-find #"\w+" uri))
+
+(defn is-allowed
+  "Checks if the user is able to do an action on a resource.
+  Returns xiana/ok when it is, and extends [:response-data :acl] with the restriction of ownership check.
+  When the user has no access, returns xiana/error or executes ((:or-else access) state) if it's provided.
+  If no 'access' has been provided, it's resolves:
+  - resource from URI (/users/ -> \"users\")
+  - and privilege from request method:
+
+  |req:    | action: |
+  |------- |---------|
+  |:get    | :read   |
+  |:post   | :update |
+  |:put    | :create |
+  |:delete | :delete |"
+  ([{:keys [user] :as state} access]
+   (let [permissions (:acl/permissions state)
+         result (if (:role user)
+                  (has-access permissions (assoc access :role (:role user)))
+                  (has-access permissions user access))]
+     (cond result (xiana/ok (assoc-in state [:response-data :acl] result))
+           (:or-else access) ((:or-else access) state)
+           :else (xiana/error (assoc state :response {:status 401 :body "Authorization error"})))))
+  ([{:keys [user http-request] :as state}]
+   (let [permissions (:acl/permissions state)
+         resource (->resource (:uri http-request))
+         privilege (action-mapping (:request-method http-request))
+         result (has-access permissions user {:resource resource :privilege privilege})]
+     (if result
+       (xiana/ok (assoc-in state [:response-data :acl] result))
+       (xiana/error (assoc state :response {:status 401 :body "Authorization error"}))))))
--- test/framework/components/acl/core_test.clj
+++ test/framework/components/acl/core_test.clj
@@ -1,7 +1,8 @@
 (ns framework.components.acl.core-test
   (:require
     [clojure.test :refer :all]
-    [framework.components.acl.core :refer [isAllowed]]))
+    [framework.components.acl.core :refer [has-access
+                                           is-allowed]]))
 
 (def config
   {:customer         [{:resource "items"
@@ -32,14 +33,14 @@
                        :actions  [:all]
                        :filter   :all}]})
 
-(deftest isAllowed-test
-  (is (= :all (isAllowed config {:role :customer :resource "items" :privilege :read})))
-  (is (false? (isAllowed config {:role :customer :resource "items" :privilege :delete})))
-  (is (= :own (isAllowed config {:role :customer :resource "users" :privilege :read})))
-  (is (= :own (isAllowed config {:role :customer :resource "users" :privilege :delete})))
-  (is (= :all (isAllowed config {:role :administrator :resource "users" :privilege :read}))))
+(deftest has-access-test
+  (is (= :all (has-access config {:role :customer :resource "items" :privilege :read})))
+  (is (false? (has-access config {:role :customer :resource "items" :privilege :delete})))
+  (is (= :own (has-access config {:role :customer :resource "users" :privilege :read})))
+  (is (= :own (has-access config {:role :customer :resource "users" :privilege :delete})))
+  (is (= :all (has-access config {:role :administrator :resource "users" :privilege :read}))))
 
-(def config-for-user-based-ussage
+(def config-for-user-based-usage
   {:guest     [{:resource "items"
                 :actions  [:read]
                 :filter   :all}]
@@ -69,14 +70,73 @@
 (def suspended-admin {:is_active false :is_superuser true})
 
 (deftest user-based-acl
-  (is (= :all (isAllowed config-for-user-based-ussage guest {:resource "items" :privilege :read})))
-  (is (false? (isAllowed config-for-user-based-ussage guest {:resource "users" :privilege :read})))
-  (is (= :own (isAllowed config-for-user-based-ussage member {:resource "users" :privilege :read})))
-  (is (= :own (isAllowed config-for-user-based-ussage member {:resource "addresses" :privilege :read})))
-  (is (false? (isAllowed config-for-user-based-ussage staff {:resource "users" :privilege :read})))
-  (is (= :all (isAllowed config-for-user-based-ussage staff {:resource "items" :privilege :update})))
-  (is (= :all (isAllowed config-for-user-based-ussage admin {:resource "items" :privilege :create})))
-  (is (= :all (isAllowed config-for-user-based-ussage admin {:resource "items" :privilege :create})))
-  (is (= :all (isAllowed config-for-user-based-ussage suspended-admin {:resource "items" :privilege :read})))
-  (is (false? (isAllowed config-for-user-based-ussage suspended-admin {:resource "items" :privilege :create})))
-  (is (false? (isAllowed config-for-user-based-ussage suspended-admin {:resource "users" :privilege :delete}))))
+  (is (= :all (has-access config-for-user-based-usage guest {:resource "items" :privilege :read})))
+  (is (false? (has-access config-for-user-based-usage guest {:resource "users" :privilege :read})))
+  (is (= :own (has-access config-for-user-based-usage member {:resource "users" :privilege :read})))
+  (is (= :own (has-access config-for-user-based-usage member {:resource "addresses" :privilege :read})))
+  (is (false? (has-access config-for-user-based-usage staff {:resource "users" :privilege :read})))
+  (is (= :all (has-access config-for-user-based-usage staff {:resource "items" :privilege :update})))
+  (is (= :all (has-access config-for-user-based-usage admin {:resource "items" :privilege :create})))
+  (is (= :all (has-access config-for-user-based-usage admin {:resource "items" :privilege :create})))
+  (is (= :all (has-access config-for-user-based-usage suspended-admin {:resource "items" :privilege :read})))
+  (is (false? (has-access config-for-user-based-usage suspended-admin {:resource "items" :privilege :create})))
+  (is (false? (has-access config-for-user-based-usage suspended-admin {:resource "users" :privilege :delete}))))
+
+(defn state-with-user
+  ([user]
+   (if (map? user)
+     {:acl/permissions config-for-user-based-usage
+      :user            user}
+     {:acl/permissions config
+      :user            {:role user}})))
+
+(defn get-ok
+  [t]
+  (get-in t [:right :response-data :acl]))
+
+(defn get-error
+  [t]
+  (get-in t [:left :response]))
+
+(deftest xiana-flow-with-access-map
+  (is (= {:status 401 :body "Authorization error"}
+         (get-error (is-allowed (state-with-user guest) {:resource "items" :privilege :create}))))
+  (is (= "Not ok 401"
+         (is-allowed (state-with-user guest) {:resource "items" :privilege :create :or-else (fn [_] (str "Not ok " 401))})))
+  (is (= :all
+         (get-ok (is-allowed (state-with-user guest) {:resource "items" :privilege :read}))))
+  (is (= {:status 401 :body "Authorization error"}
+         (get-error (is-allowed (state-with-user guest) {:resource "items" :privilege :update}))))
+  (is (= :own
+         (get-ok (is-allowed (state-with-user member) {:resource "users" :privilege :update}))))
+  (is (= :own
+         (get-ok (is-allowed (state-with-user :customer) {:resource "users" :privilege :update}))))
+  (is (= :all
+         (get-ok (is-allowed (state-with-user :administrator) {:resource "users" :privilege :create}))))
+  (is (= :all
+         (get-ok (is-allowed (state-with-user :administrator) {:resource "users" :privilege :delete})))))
+
+(defn state-with-user-request
+  [user uri method]
+  {:acl/permissions config-for-user-based-usage
+   :user            user
+   :http-request    {:uri            uri
+                     :request-method method}})
+
+(deftest xiana-flow-from-request-only
+  (is (= :all
+         (get-ok (is-allowed (state-with-user-request guest "/items/" :get)))))
+  (is (= {:status 401, :body "Authorization error"}
+         (get-error (is-allowed (state-with-user-request guest "/items/" :post)))))
+  (is (= :own
+         (get-ok (is-allowed (state-with-user-request member "/addresses/" :post)))))
+  (is (= :all
+         (get-ok (is-allowed (state-with-user-request admin "/items/" :get)))))
+  (is (= :all
+         (get-ok (is-allowed (state-with-user-request admin "/items/" :put)))))
+  (is (= :all
+         (get-ok (is-allowed (state-with-user-request admin "/items/" :post)))))
+  (is (= :all
+         (get-ok (is-allowed (state-with-user-request suspended-admin "/items/" :get)))))
+  (is (= {:status 401, :body "Authorization error"}
+         (get-error (is-allowed (state-with-user-request suspended-admin "/items/" :put))))))
