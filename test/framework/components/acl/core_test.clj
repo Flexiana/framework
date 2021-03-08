35a6167efe17bb4308c2058d6ced79b5bc5f0d3a
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-03-08 11:30:56 +0100

    extract permissions from config

--- test/framework/components/acl/core_test.clj
+++ test/framework/components/acl/core_test.clj
@@ -4,33 +4,33 @@
     [framework.components.acl.core :refer [isAllowed]]))
 
 (def config
-  {:users/permissions {:customer         [{:resource "items"
-                                           :actions  [:read]
-                                           :filter   :all}
-                                          {:resource "users"
-                                           :actions  [:read :update :delete]
-                                           :filter   :own}
-                                          {:resource "addresses"
-                                           :actions  [:create :read :update :delete]
-                                           :filter   :own}
-                                          {:resource "carts"
-                                           :actions  [:create :read :update :delete]
-                                           :filter   :own}]
-                       :warehouse-worker [{:resource "items"
-                                           :actions  [:read :update]
-                                           :filter   :all}]
-                       :postal-worker    [{:resource "carts"
-                                           :actions  [:read :update]
-                                           :filter   :all}
-                                          {:resource "addresses"
-                                           :actions  [:read]
-                                           :filter   :all}]
-                       :shop-worker      [{:resource "items"
-                                           :actions  [:all]
-                                           :filter   :all}]
-                       :administrator    [{:resource :all
-                                           :actions  [:all]
-                                           :filter   :all}]}})
+  {:customer         [{:resource "items"
+                       :actions  [:read]
+                       :filter   :all}
+                      {:resource "users"
+                       :actions  [:read :update :delete]
+                       :filter   :own}
+                      {:resource "addresses"
+                       :actions  [:create :read :update :delete]
+                       :filter   :own}
+                      {:resource "carts"
+                       :actions  [:create :read :update :delete]
+                       :filter   :own}]
+   :warehouse-worker [{:resource "items"
+                       :actions  [:read :update]
+                       :filter   :all}]
+   :postal-worker    [{:resource "carts"
+                       :actions  [:read :update]
+                       :filter   :all}
+                      {:resource "addresses"
+                       :actions  [:read]
+                       :filter   :all}]
+   :shop-worker      [{:resource "items"
+                       :actions  [:all]
+                       :filter   :all}]
+   :administrator    [{:resource :all
+                       :actions  [:all]
+                       :filter   :all}]})
 
 (deftest isAllowed-test
   (is (= :all (isAllowed config {:role :customer :resource "items" :privilege :read})))
@@ -40,27 +40,27 @@
   (is (= :all (isAllowed config {:role :administrator :resource "users" :privilege :read}))))
 
 (def config-for-user-based-ussage
-  {:users/permissions {:guest     [{:resource "items"
-                                    :actions  [:read]
-                                    :filter   :all}]
-                       :member    [{:resource "items"
-                                    :actions  [:read]
-                                    :filter   :all}
-                                   {:resource "users"
-                                    :actions  [:read :update :delete]
-                                    :filter   :own}
-                                   {:resource "addresses"
-                                    :actions  [:create :read :update :delete]
-                                    :filter   :own}
-                                   {:resource "carts"
-                                    :actions  [:create :read :update :delete]
-                                    :filter   :own}]
-                       :staff     [{:resource "items"
-                                    :actions  [:read :update]
-                                    :filter   :all}]
-                       :superuser [{:resource :all
-                                    :actions  [:all]
-                                    :filter   :all}]}})
+  {:guest     [{:resource "items"
+                :actions  [:read]
+                :filter   :all}]
+   :member    [{:resource "items"
+                :actions  [:read]
+                :filter   :all}
+               {:resource "users"
+                :actions  [:read :update :delete]
+                :filter   :own}
+               {:resource "addresses"
+                :actions  [:create :read :update :delete]
+                :filter   :own}
+               {:resource "carts"
+                :actions  [:create :read :update :delete]
+                :filter   :own}]
+   :staff     [{:resource "items"
+                :actions  [:read :update]
+                :filter   :all}]
+   :superuser [{:resource :all
+                :actions  [:all]
+                :filter   :all}]})
 
 (def guest {})
 (def member {:is_active true})
