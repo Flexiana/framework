40a349cc0eec6921696920daa3f7fa430e54418e
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-03-08 11:02:55 +0100

    User provided to ACL instead of role

--- src/framework/components/acl/core.clj
+++ src/framework/components/acl/core.clj
@@ -1,11 +1,17 @@
 (ns framework.components.acl.core)
 
 (defn isAllowed
-  [{permissions :users/permissions} {:keys [role resource privilege]}]
-  (let [owning (->> (get permissions role)
-                    (filter #(#{resource :all} (:resource %)))
-                    first)]
-    (if (some #{privilege :all} (:actions owning))
-      (:filter owning)
-      false)))
+  ([config user access]
+   (cond
+     (not (:is_active user)) (isAllowed config (assoc access :role :guest))
+     (:is_superuser user) (isAllowed config (assoc access :role :superuser))
+     (:is_staff user) (isAllowed config (assoc access :role :staff))
+     (:is_active user) (isAllowed config (assoc access :role :member))))
+  ([{permissions :users/permissions} {:keys [role resource privilege]}]
+   (let [granted (->> (get permissions role)
+                      (filter #(#{resource :all} (:resource %)))
+                      first)]
+     (if (some #{privilege :all} (:actions granted))
+       (:filter granted)
+       false))))
 
--- test/framework/components/acl/core_test.clj
+++ test/framework/components/acl/core_test.clj
@@ -38,3 +38,45 @@
   (is (= :own (isAllowed config {:role :customer :resource "users" :privilege :read})))
   (is (= :own (isAllowed config {:role :customer :resource "users" :privilege :delete})))
   (is (= :all (isAllowed config {:role :administrator :resource "users" :privilege :read}))))
+
+(def config-for-user-based-ussage
+  {:users/permissions {:guest     [{:resource "items"
+                                    :actions  [:read]
+                                    :filter   :all}]
+                       :member    [{:resource "items"
+                                    :actions  [:read]
+                                    :filter   :all}
+                                   {:resource "users"
+                                    :actions  [:read :update :delete]
+                                    :filter   :own}
+                                   {:resource "addresses"
+                                    :actions  [:create :read :update :delete]
+                                    :filter   :own}
+                                   {:resource "carts"
+                                    :actions  [:create :read :update :delete]
+                                    :filter   :own}]
+                       :staff     [{:resource "items"
+                                    :actions  [:read :update]
+                                    :filter   :all}]
+                       :superuser [{:resource :all
+                                    :actions  [:all]
+                                    :filter   :all}]}})
+
+(def guest {})
+(def member {:is_active true})
+(def staff {:is_active true :is_staff true})
+(def admin {:is_active true :is_superuser true})
+(def suspended-admin {:is_active false :is_superuser true})
+
+(deftest user-based-acl
+  (is (= :all (isAllowed config-for-user-based-ussage guest {:resource "items" :privilege :read})))
+  (is (false? (isAllowed config-for-user-based-ussage guest {:resource "users" :privilege :read})))
+  (is (= :own (isAllowed config-for-user-based-ussage member {:resource "users" :privilege :read})))
+  (is (= :own (isAllowed config-for-user-based-ussage member {:resource "addresses" :privilege :read})))
+  (is (false? (isAllowed config-for-user-based-ussage staff {:resource "users" :privilege :read})))
+  (is (= :all (isAllowed config-for-user-based-ussage staff {:resource "items" :privilege :update})))
+  (is (= :all (isAllowed config-for-user-based-ussage admin {:resource "items" :privilege :create})))
+  (is (= :all (isAllowed config-for-user-based-ussage admin {:resource "items" :privilege :create})))
+  (is (= :all (isAllowed config-for-user-based-ussage suspended-admin {:resource "items" :privilege :read})))
+  (is (false? (isAllowed config-for-user-based-ussage suspended-admin {:resource "items" :privilege :create})))
+  (is (false? (isAllowed config-for-user-based-ussage suspended-admin {:resource "users" :privilege :delete}))))
