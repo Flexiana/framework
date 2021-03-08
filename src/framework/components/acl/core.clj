49b18cdf94fa31187b5b7e7bffd9df2c9ab3808b
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-03-08 11:31:10 +0100

    add docstring

--- src/framework/components/acl/core.clj
+++ src/framework/components/acl/core.clj
@@ -1,7 +1,14 @@
 (ns framework.components.acl.core)
 
 (defn isAllowed
-  ([config user access]
+  "Examine if the user is able to access to a resource with the provided action.
+  If it's able to, returns anything what is provided in 'permissions' corresponding :filter field.
+  If it's isn't then returns \"false\"
+  'permissions' is a map keyed by name of permissions.
+  'user' is optional, but if it missing you must provide the 'role' field in action.
+  'access' defines the role, resource and privilege what needs to be achieved.
+  If user is provided, the role will be resolved from it."
+  ([permissions user access]
    (cond
      (not (:is_active user)) (isAllowed permissions (assoc access :role :guest))
      (:is_superuser user) (isAllowed permissions (assoc access :role :superuser))
