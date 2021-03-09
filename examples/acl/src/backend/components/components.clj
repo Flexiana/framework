54e8923d16b2e344f1b2867d32b00b308ef86c68
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-03-09 17:10:36 +0100

    skeleton of posts endpoint

--- examples/acl/src/backend/components/components.clj
+++ examples/acl/src/backend/components/components.clj
@@ -10,17 +10,19 @@
   [config]
   (let [pg-cfg (:framework.db.storage/postgresql config)
         app-cfg (:framework.app/ring config)
-        web-server-cfg (:framework.app/web-server config)]
+        web-server-cfg (:framework.app/web-server config)
+        acl-permissions (:acl/permissions config)]
     (->
       (component/system-map
         :config config
         :db (db.storage/postgresql pg-cfg)
         :router (router/make-router)
         :app (app/make-app app-cfg)
+        :acl-permissions acl-permissions
         :web-server (web-server/make-web-server web-server-cfg))
       (component/system-using
         {:router     [:db]
-         :app        [:router :db]
+         :app        [:router :db :acl-permissions]
          :web-server [:app]}))))
 
 (defn -main
