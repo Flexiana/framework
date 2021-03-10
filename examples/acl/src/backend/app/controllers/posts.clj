68c2c1cff4857541fe9b0e3f641d1004af350ba4
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-03-10 13:56:40 +0100

    add a method based routing

--- examples/acl/src/backend/app/controllers/posts.clj
+++ examples/acl/src/backend/app/controllers/posts.clj
@@ -1,17 +1,24 @@
 (ns controllers.posts
-  (:require [xiana.core :as xiana]
-            [framework.components.acl.core :as acl]
-            [views.posts :as views]
-            [honeysql.core :as sql]
-            [honeysql.helpers :refer :all :as helpers]
-            [next.jdbc :as jdbc]
-            [clojure.string :as str])
-  (:import (java.util UUID)))
+  (:require
+    [clojure.string :as str]
+    [framework.components.acl.core :as acl]
+    [honeysql.core :as sql]
+    [honeysql.helpers :refer :all :as helpers]
+    [next.jdbc :as jdbc]
+    [ring.middleware.params :as par]
+    [ring.util.request :as rreq]
+    [views.posts :as views]
+    [xiana.core :as xiana])
+  (:import
+    (java.util
+      UUID)))
 
-(defn unauthorized [state]
+(defn unauthorized
+  [state]
   (xiana/error (assoc state :response {:status 401 :body "Unauthorized"})))
 
-(defn require-logged-in [{req :http-request :as state}]
+(defn require-logged-in
+  [{req :http-request :as state}]
   (if-let [authorization (get-in req [:headers "authorization"])]
     (try (xiana/ok (-> (assoc-in state [:session-data :authorization] authorization)
                        (assoc-in [:session :user :id] (UUID/fromString authorization))))
@@ -20,30 +27,60 @@
 
 (defn something-else
   [state]
+  (println (rreq/body-string (:http-request state)))
+  (println (par/params-request (:http-request state)))
   (xiana/ok state))
 
 (defn execute
   [state query]
   (jdbc/execute! (get-in state [:deps :db :datasource]) query))
 
-(defn purify [elem]
+(defn purify
+  [elem]
   (into {} (map (fn [[k v]] {(keyword (last (str/split (name k) #"/"))) v}) elem)))
 
-(defn fetch-user [{{{id :id} :user} :session :as state}]
+(defn fetch-user
+  [{{{id :id} :user} :session :as state}]
   (let [query (-> (select :*)
                   (from :users)
                   (where [:= :id id])
                   sql/format)
         user (first (execute state query))]
     (if user (xiana/ok (assoc-in state [:session :user] (purify user)))
-             (xiana/error (assoc state :response {:status 404 :body "User not found"})))))
+        (xiana/error (assoc state :response {:status 404 :body "User not found"})))))
 
+(defn fetch-posts
+  [state]
+  (xiana/ok state))
 
-(defn controller [state]
+(defn update-post
+  [state]
+  (xiana/ok state))
+
+(defn create-post
+  [state]
+  (xiana/ok state))
+
+(defn delete-post
+  [state]
+  (xiana/ok state))
+
+(defn re-router
+  [{{method :request-method} :http-request :as state}]
+  (println method)
+  (case method
+    :get (fetch-posts state)
+    :post (update-post state)
+    :put (create-post state)
+    :delete (delete-post state)))
+
+(defn controller
+  [state]
   (xiana/flow->
     state
     (require-logged-in)
     (fetch-user)
     (acl/is-allowed)
     (something-else)
+    (re-router)
     (views/posts-view)))
