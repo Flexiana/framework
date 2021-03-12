cce5b90df06684f8cc23e219e8ef33e76c33e486
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-03-12 13:42:49 +0100

    add tests to devtest

--- test/framework/components/acl/builder/roles_test.clj
+++ test/framework/components/acl/builder/roles_test.clj
@@ -13,67 +13,59 @@
                       :right
                       :acl/roles))))
 
-(deftest init-test)
-
-(test-roles {}
-            (xiana/flow->
-              {}
-              init))
-
-(test-roles {:guest [{:resource "posts", :actions [:blow], :restriction :own}
-                     {:resource "posts", :actions [:read :delete], :restriction :all}]}
-            (xiana/flow->
-              {}
-              init
-              (allow {:role :guest :resource "posts" :actions :read})
-              (allow {:role :guest :resource "posts" :actions :delete})
-              (allow {:role :guest :resource "posts" :actions :blow :restriction :own})))
-
-(test-roles {:guest [{:resource "posts", :actions [:read :delete], :restriction :all}]}
-            (xiana/flow->
-              {}
-              (p/init {"posts" [:read :delete :blow :blow-up]})
-              (init {:guest [{:resource "posts", :actions [:blow], :restriction :own}
-                             {:resource "posts", :actions [:read :delete], :restriction :all}]})
-              (deny {:role :guest :resource "posts" :actions :blow})))
-
-(test-roles {}
-            (xiana/flow->
-              {}
-              (p/init {"posts" [:read :delete :blow :blow-up]})
-              (init {:guest [{:resource "posts", :actions [:blow], :restriction :own}
-                             {:resource "posts", :actions [:read :delete], :restriction :all}]})
-              (deny {:role :guest :resource "posts" :actions :all})))
-
-(test-roles {:guest [{:resource "posts", :actions [:read :blow :blow-up], :restriction :own}]}
-            (xiana/flow->
-              {}
-              (p/init {"posts" [:read :delete :blow :blow-up]})
-              (init {:guest [{:resource "posts", :actions [:all], :restriction :own}]})
-              (deny {:role :guest :resource "posts" :actions :delete})))
-
-(test-roles {:guest [{:resource "posts", :actions [:read], :restriction :own}]
-             :member [{:resource "posts", :actions [:delete :read], :restriction :own}]}
-            (xiana/flow->
-              {}
-              ;(p/init {"posts" [:read :delete :blow :blow-up]})
-              (init {:guest  [{:resource "posts", :actions [:delete :read], :restriction :own}]
-                     :member [{:resource "posts", :actions [:delete :read], :restriction :own}]})
-              (deny {:role :guest :resource "posts" :actions :delete})))
-
-(test-roles {:member [{:resource "posts", :actions [:delete :read], :restriction :own}]}
-            (xiana/flow->
-              {}
-              ;(p/init {"posts" [:read :delete :blow :blow-up]})
-              (init {:guest  [{:resource "posts", :actions [:all], :restriction :own}]
-                     :member [{:resource "posts", :actions [:delete :read], :restriction :own}]})
-              (deny {:role :guest :resource "posts" :actions :delete})))
-
-(test-roles {:guest  [{:resource "posts", :actions [:read :blow :blow-up], :restriction :own}]
-             :member [{:resource "posts", :actions [:delete :read], :restriction :own}]}
-            (xiana/flow->
-              {}
-              (p/init {"posts" [:read :delete :blow :blow-up]})
-              (init {:guest  [{:resource "posts", :actions [:all], :restriction :own}]
-                     :member [{:resource "posts", :actions [:delete :read], :restriction :own}]})
-              (deny {:role :guest :resource "posts" :actions :delete})))
+(deftest init-test
+  (test-roles {}
+              (xiana/flow->
+                {}
+                init))
+  (test-roles {:guest [{:resource "posts", :actions [:blow], :restriction :own}
+                       {:resource "posts", :actions [:read :delete], :restriction :all}]}
+              (xiana/flow->
+                {}
+                init
+                (allow {:role :guest :resource "posts" :actions :read})
+                (allow {:role :guest :resource "posts" :actions :delete})
+                (allow {:role :guest :resource "posts" :actions :blow :restriction :own})))
+  (test-roles {:guest [{:resource "posts", :actions [:read :delete], :restriction :all}]}
+              (xiana/flow->
+                {}
+                (p/init {"posts" [:read :delete :blow :blow-up]})
+                (init {:guest [{:resource "posts", :actions [:blow], :restriction :own}
+                               {:resource "posts", :actions [:read :delete], :restriction :all}]})
+                (deny {:role :guest :resource "posts" :actions :blow})))
+  (test-roles {}
+              (xiana/flow->
+                {}
+                (p/init {"posts" [:read :delete :blow :blow-up]})
+                (init {:guest [{:resource "posts", :actions [:blow], :restriction :own}
+                               {:resource "posts", :actions [:read :delete], :restriction :all}]})
+                (deny {:role :guest :resource "posts" :actions :all})))
+  (test-roles {:guest [{:resource "posts", :actions [:read :blow :blow-up], :restriction :own}]}
+              (xiana/flow->
+                {}
+                (p/init {"posts" [:read :delete :blow :blow-up]})
+                (init {:guest [{:resource "posts", :actions [:all], :restriction :own}]})
+                (deny {:role :guest :resource "posts" :actions :delete})))
+  (test-roles {:guest [{:resource "posts", :actions [:read], :restriction :own}]
+               :member [{:resource "posts", :actions [:delete :read], :restriction :own}]}
+              (xiana/flow->
+                {}
+                ;(p/init {"posts" [:read :delete :blow :blow-up]})
+                (init {:guest  [{:resource "posts", :actions [:delete :read], :restriction :own}]
+                       :member [{:resource "posts", :actions [:delete :read], :restriction :own}]})
+                (deny {:role :guest :resource "posts" :actions :delete})))
+  (test-roles {:member [{:resource "posts", :actions [:delete :read], :restriction :own}]}
+              (xiana/flow->
+                {}
+                ;(p/init {"posts" [:read :delete :blow :blow-up]})
+                (init {:guest  [{:resource "posts", :actions [:all], :restriction :own}]
+                       :member [{:resource "posts", :actions [:delete :read], :restriction :own}]})
+                (deny {:role :guest :resource "posts" :actions :delete})))
+  (test-roles {:guest  [{:resource "posts", :actions [:read :blow :blow-up], :restriction :own}]
+               :member [{:resource "posts", :actions [:delete :read], :restriction :own}]}
+              (xiana/flow->
+                {}
+                (p/init {"posts" [:read :delete :blow :blow-up]})
+                (init {:guest  [{:resource "posts", :actions [:all], :restriction :own}]
+                       :member [{:resource "posts", :actions [:delete :read], :restriction :own}]})
+                (deny {:role :guest :resource "posts" :actions :delete}))))
