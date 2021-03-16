4d52a764c12a4fd05ff2e5b00973a41ef23beba0
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-03-16 21:18:22 +0100

    add docstrings

--- examples/acl/src/backend/app/controllers/posts.clj
+++ examples/acl/src/backend/app/controllers/posts.clj
@@ -20,6 +20,7 @@
   (xiana/error (assoc state :response {:status 401 :body "Unauthorized"})))
 
 (defn require-logged-in
+  "Tricky login, session should handle user data"
   [{req :http-request :as state}]
   (if-let [authorization (get-in req [:headers "authorization"])]
     (try (xiana/ok (-> (assoc-in state [:session-data :authorization] authorization)
@@ -28,18 +29,22 @@
     (unauthorized state)))
 
 (defn add-params
+  "Extract parameters from request, should be middleware, or interceptor"
   [state]
   (xiana/ok (clojure.core/update state :http-request #(keywordize-keys ((par/wrap-params identity) %)))))
 
 (defn execute
+  "Executes db query"
   [state query]
   (jdbc/execute! (get-in state [:deps :db :datasource]) query))
 
 (defn purify
+  "Removes namespaces from keywords"
   [elem]
   (into {} (map (fn [[k v]] {(keyword (last (str/split (name k) #"/"))) v}) elem)))
 
 (defn fetch-user
+  "Tricky login. Session should handle user data"
   [{{{id :id} :user} :session :as state}]
   (let [query (-> (select :*)
                   (from :users)
@@ -80,10 +85,12 @@
    :delete delete-post})
 
 (defn select-view
+  "Inserts view, depends on request-method"
   [{{method :request-method} :http-request :as state}]
   (xiana/ok (assoc state :view (view-map method))))
 
 (def query-map
+  "Basic SQL queries by request-method"
   {:get    (-> (select :*)
                (from :posts))
    :post   (helpers/update :posts)
@@ -91,10 +98,12 @@
    :delete (delete-from :posts)})
 
 (defn base-query
+  "Inserting SQL query into state"
   [{{method :request-method} :http-request :as state}]
   (xiana/ok (assoc state :query (query-map method))))
 
 (defn handle-id
+  "Add where clause to SQL query if it's present"
   [{{params :params} :http-request query :query :as state}]
   (println "handle-id" params)
   (xiana/ok (if (:id params)
@@ -102,17 +111,20 @@
               state)))
 
 (defn view
+  "Prepare and run view"
   [{view :view :as state}]
   (println (:query state))
   (view state))
 
 (defn restriction
+  "Extends WHERE clause if necessary, to check data-ownership"
   [{{restriction :acl} :response-data query :query :as state}]
   (case restriction
     :own (xiana/ok (assoc state :query (-> query (merge-where [:= :user_id (get-in state [:session :user :id])]))))
     (xiana/ok state)))
 
 (defn controller
+  "Example controller for ACL, and DataOwnership"
   [state]
   (xiana/flow->
     state
@@ -125,5 +137,3 @@
     handle-id
     restriction
     view))
-
-
