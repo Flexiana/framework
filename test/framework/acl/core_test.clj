33d15d28eb8f0dfc5f004e683f5293275a6fb62f
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-03-30 11:42:53 +0200

    support multi behaviors - tests

--- test/framework/acl/core_test.clj
+++ test/framework/acl/core_test.clj
@@ -63,9 +63,9 @@
 (defn state-with-user
   ([user]
    (if (map? user)
-     {:acl/roles default-roles
+     {:acl/roles    default-roles
       :session-data {:user user}}
-     {:acl/roles custom-roles
+     {:acl/roles    custom-roles
       :session-data {:user {:role user}}})))
 
 (defn get-ok
@@ -81,59 +81,57 @@
          (get-error (is-allowed (state-with-user guest) {:resource "items" :privilege :create}))))
   (is (= "Not ok 401"
          (is-allowed (state-with-user guest) {:resource "items" :privilege :create :or-else (fn [_] (str "Not ok " 401))})))
-  (is (= :all
+  (is (= {:items :all}
          (get-ok (is-allowed (state-with-user guest) {:resource "items" :privilege :read}))))
   (is (= {:status 401 :body "Authorization error"}
          (get-error (is-allowed (state-with-user guest) {:resource "items" :privilege :update}))))
-  (is (= :own
+  (is (= {:users :own}
          (get-ok (is-allowed (state-with-user member) {:resource "users" :privilege :update}))))
-  (is (= :own
+  (is (= {:users :own}
          (get-ok (is-allowed (state-with-user :customer) {:resource "users" :privilege :update}))))
-  (is (= :all
+  (is (= {:users :all}
          (get-ok (is-allowed (state-with-user :administrator) {:resource "users" :privilege :create}))))
-  (is (= :all
+  (is (= {:users :all}
          (get-ok (is-allowed (state-with-user :administrator) {:resource "users" :privilege :delete})))))
 
 (defn state-with-user-request
   [user uri method]
   (if (map? user)
-    {:acl/roles default-roles
+    {:acl/roles    default-roles
      :session-data {:user user}
-     :request   {:uri            uri
-                 :request-method method}}
-    {:acl/roles custom-roles
+     :request      {:uri            uri
+                    :request-method method}}
+    {:acl/roles    custom-roles
      :session-data {:user {:role user}}
-     :request   {:uri            uri
-                 :request-method method}}))
+     :request      {:uri            uri
+                    :request-method method}}))
 
 (deftest xiana-flow-from-request-only
-  (is (= :all
+  (is (= {:items :all}
          (get-ok (is-allowed (state-with-user-request guest "/items/" :get)))))
   (is (= {:status 401, :body "Authorization error"}
          (get-error (is-allowed (state-with-user-request guest "/items/" :post)))))
-  (is (= :own
+  (is (= {:addresses :own}
          (get-ok (is-allowed (state-with-user-request member "/addresses/" :post)))))
-  (is (= :all
+  (is (= {:items :all}
          (get-ok (is-allowed (state-with-user-request admin "/items/" :get)))))
-  (is (= :all
+  (is (= {:items :all}
          (get-ok (is-allowed (state-with-user-request admin "/items/" :put)))))
-  (is (= :all
+  (is (= {:items :all}
          (get-ok (is-allowed (state-with-user-request admin "/items/" :post)))))
-  (is (= :all
+  (is (= {:items :all}
          (get-ok (is-allowed (state-with-user-request suspended-admin "/items/" :get)))))
   (is (= {:status 401, :body "Authorization error"}
          (get-error (is-allowed (state-with-user-request suspended-admin "/items/" :put)))))
-  (is (= :all
+  (is (= {:items :all}
          (get-ok (is-allowed (state-with-user-request :customer "/items/" :get)))))
   (is (= {:status 401, :body "Authorization error"}
          (get-error (is-allowed (state-with-user-request :customer "/items/" :post)))))
-  (is (= :own
+  (is (= {:addresses :own}
          (get-ok (is-allowed (state-with-user-request :customer "/addresses/" :post)))))
-  (is (= :all
+  (is (= {:items :all}
          (get-ok (is-allowed (state-with-user-request :administrator "/items/" :get)))))
-  (is (= :all
+  (is (= {:items :all}
          (get-ok (is-allowed (state-with-user-request :administrator "/items/" :put)))))
-  (is (= :all
+  (is (= {:items :all}
          (get-ok (is-allowed (state-with-user-request :administrator "/items/" :post))))))
-
-
