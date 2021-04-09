b3e6c3f06a6f064a0241db3a9f40268daa76276e
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-04-09 16:03:55 +0200

    refactor ->store-user

--- examples/acl/src/backend/app/models/users.clj
+++ examples/acl/src/backend/app/models/users.clj
@@ -9,23 +9,19 @@
 
 (defn ->store-user
   [m]
-  (let [u (select-keys m [:id
-                          :password
-                          :last_login
-                          :is_superuser
-                          :username
-                          :first_name
-                          :last_name
-                          :email
-                          :is_staff
-                          :is_active])]
-    (cond-> u
-      (:is_active u) (assoc :is_active true)
-      (:is_staff u) (assoc :is_staff true)
-      (:is_superuser u) (assoc :is_superuser true)
-      (nil? (:is_active u)) (assoc :is_active false)
-      (nil? (:is_staff u)) (assoc :is_staff false)
-      (nil? (:is_superuser u)) (assoc :is_superuser false))))
+  (->> (select-keys m [:id
+                       :password
+                       :last_login
+                       :is_superuser
+                       :username
+                       :first_name
+                       :last_name
+                       :email
+                       :is_staff
+                       :is_active])
+       (into {:is_active    true
+              :is_staff     false
+              :is_superuser false})))
 
 (defn fetch-query
   [{{{id :id} :query-params} :request
