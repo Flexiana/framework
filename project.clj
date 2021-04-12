db9f7d34dd48b349343001ceac672dca8bae42f9
Ian Fernandez <d.ian.b@live.com>
2021-04-12 06:55:15 -0300

    Bump project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.flexiana/framework "0.1.5"
+(defproject com.flexiana/framework "0.1.6"
   :description "Framework"
   :url "https://github.com/Flexiana/framework"
   :license {:name "FIXME" :url "FIXME"}
