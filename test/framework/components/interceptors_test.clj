9a8635c5aeec6121d247b10b8d09390239edc113
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-04-13 15:08:03 +0200

    cljstyle

--- test/framework/components/interceptors_test.clj
+++ test/framework/components/interceptors_test.clj
@@ -1,12 +1,11 @@
 (ns framework.components.interceptors-test
-  (:require [clojure.test :refer :all])
-  (:require [framework.components.interceptors :as interceptors]
-            [framework.components.session.backend :refer [dump]]
-            [framework.components.session.backend :as session-backend]))
-
+  (:require
+    [clojure.test :refer :all]
+    [framework.components.interceptors :as interceptors]
+    [framework.components.session.backend :refer [dump]]
+    [framework.components.session.backend :as session-backend]))
 
 (def session-id #uuid"21f0d6e6-3782-465a-b903-ca84f6f581a0")
-
 
 (deftest store-fetch-session-via-interceptor
   (let [session-bcknd (assoc-in {} [:deps :session-backend] (session-backend/init-in-memory-session))
@@ -16,6 +15,6 @@
         session (get-in response [:response :headers "Session-id"])
         request (assoc-in session-bcknd [:request :headers :session-id] session)
         new-state ((:enter interceptors/session-interceptor) request)]
-    (is (= {:user {:id 1, :name "john"}
+    (is (= {:user       {:id 1, :name "john"}
             :session-id #uuid"21f0d6e6-3782-465a-b903-ca84f6f581a0"}
            (-> new-state :right :session-data)))))
