6adb91fc79660ff5b3bbdc04ebb7e6b7ec7b8e1d
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-04-13 15:04:29 +0200

    Testing session interceptor to store/fetch anything in session

--- src/framework/components/interceptors.clj
+++ src/framework/components/interceptors.clj
@@ -43,10 +43,11 @@
             (let [sessions-backend (-> state
                                        :deps
                                        :session-backend)
-                  session-id (get-in request [:headers :session-id])
-                  user (when session-id (fetch sessions-backend session-id))]
+                  session-id (UUID/fromString (get-in request [:headers :session-id]))
+                  user (when session-id (:user (fetch sessions-backend session-id)))]
               (if user
-                (xiana/ok (assoc-in state [:session-data :user] user))
+                (xiana/ok (assoc state :session-data {:user user
+                                                      :session-id session-id}))
                 (xiana/ok (-> (assoc-in state [:session-data :session-id] (UUID/randomUUID))
                               (assoc-in [:session-data :new-session] true))))))
 
@@ -55,7 +56,7 @@
                                        :deps
                                        :session-backend)
                   session-id (get-in state [:session-data :session-id])]
-              (add! sessions-backend session-id (get-in state [:session-data :user]))
+              (add! sessions-backend session-id (:session-data state))
               (xiana/ok (assoc-in state [:response :headers "Session-id"] (str session-id)))))})
 
 (def params
