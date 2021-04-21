f36ecaf786cee4db468ecd7262b4ebec43d7a688
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-04-21 11:37:08 +0200

    Update runner test

--- test/framework/components/runner_test.clj
+++ test/framework/components/runner_test.clj
@@ -1,28 +1,34 @@
 (ns framework.components.runner-test
   (:require
     [clojure.test :refer :all]
-    [framework.components.runner :refer [run]]))
+    [framework.components.runner :refer [run]]
+    [xiana.core :as xiana]))
 
 (deftest runner-test
-  (is (= {:enter/first true
-          :enter/second true
-          :action :action
-          :leave/second true
-          :leave/first true}
-         (run {} [{:enter (fn [state] (assoc state :enter/first true))
-                   :leave (fn [state] (assoc state :leave/first true))}
-                  {:enter (fn [state] (assoc state :enter/second true))
-                   :leave (fn [state] (assoc state :leave/second true))}]
-              #(assoc % :action :action)))))
+  (is (= {:enter/first true, :enter/second true, :action :action, :leave/second true, :leave/first true}
+         (:right
+           (xiana/flow->
+             {}
+             (run  [{:enter (fn [state] (xiana/ok (assoc state :enter/first true)))
+                     :leave (fn [state] (xiana/ok (assoc state :leave/first true)))}
+                    {:enter (fn [state] (xiana/ok (assoc state :enter/second true)))
+                     :leave (fn [state] (xiana/ok (assoc state :leave/second true)))}]
+                   #(xiana/ok (assoc % :action :action))))))))
 
 (deftest error-test
   (is (= {:error "something wrong"}
-         (run {} [{:enter (fn [state] (update state :num inc))
-                   :error (fn [state] (assoc state :error "something wrong"))
-                   :leave (fn [state] (assoc state :num dec))}]
-              #(assoc % :action :action))))
+         (:right
+           (xiana/flow->
+             {}
+             (run [{:enter (fn [state] (xiana/ok (update state :num inc)))
+                    :error (fn [state] (xiana/ok (assoc state :error "something wrong")))
+                    :leave (fn [state] (xiana/ok (assoc state :num dec)))}]
+                  #(xiana/ok (assoc % :action :action)))))))
   (is (= {:num 0, :action :action}
-         (run {:num 0} [{:enter (fn [state] (update state :num inc))
-                         :error (fn [state] (assoc state :error "something wrong"))
-                         :leave (fn [state] (update state :num dec))}]
-              #(assoc % :action :action)))))
+         (:right
+           (xiana/flow->
+             {:num 0}
+             (run  [{:enter (fn [state] (xiana/ok (update state :num inc)))
+                     :error (fn [state] (xiana/ok (assoc state :error "something wrong")))
+                     :leave (fn [state] (xiana/ok (update state :num dec)))}]
+                   #(xiana/ok (assoc % :action :action))))))))
