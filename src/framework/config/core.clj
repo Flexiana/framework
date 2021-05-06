1059249f32bf56e4053cfd07a43961d3b091aa3e
lambdart <lambdart@protonmail.com>
2021-05-06 14:08:29 -0300

    Remove components and add read-edn-file, get-spec functions

--- src/framework/config/core.clj
+++ src/framework/config/core.clj
@@ -1,18 +1,30 @@
 (ns framework.config.core
   (:require
-    [com.stuartsierra.component :as component]
-    [config.core :refer [load-env]]))
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]))
 
-(defrecord Config
-  []
-  component/Lifecycle
-  (start [_this]
-         (load-env)))
+;; default edn file
+(def default-edn-file
+  (.getAbsolutePath (java.io.File. "config/dev/config.edn")))
 
-(defn new-config
-  []
-  (->Config))
+;; default config map: util wrapper/translator
+(def default-config-map
+  {:acl       :framework.app/acl
+   :auth      :framework.app/auth
+   :emails    :framework.app/emails
+   :webserver :framework.app/web-server
+   :migration :framework.db.storage/migration
+   :database  :framework.db.storage/postgresql})
 
-(defn edn
-  []
-  (load-env))
+(defn read-edn-file
+  "Read edn configuration file."
+  [edn-file]
+  (with-open [r (io/reader (or edn-file default-edn-file))]
+    (edn/read (java.io.PushbackReader. r))))
+
+(defn get-spec
+  "Select configuration spec using 'k' identifier."
+  ([k] (get-spec k nil))
+  ([k edn-file]
+   (get (read-edn-file edn-file)
+        (-> k default-config-map))))
