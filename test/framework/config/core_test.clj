c07fbb36529c5f397a20285a90a8e2306b2fd18d
lambdart <lambdart@protonmail.com>
2021-05-07 14:09:17 -0300

    Rename and add more tests

--- test/framework/config/core_test.clj
+++ test/framework/config/core_test.clj
@@ -5,8 +5,34 @@
 
 (def config-map (config/read-edn-file nil))
 
-(deftest its-key-present
-  (is (:framework.db.storage/postgresql config-map)))
+;; test if the configuration map is not empty
+(deftest config-map-not-empty
+  (is (= (empty? config-map) false)))
 
-(deftest its-key-not-present
+;; test if contains an arbitrary key that should not be present
+;; if equal to nil -> true, otherwise false
+(deftest wrong-key-not-present
   (is (= (:non-existent-key config-map) nil)))
+
+;; test if the default keys are present
+(deftest default-keys-are-present
+  (is (not (= (and
+               (:framework.app/web-server config-map)
+               (:framework.db.storage/postgresql config-map)
+               (:framework.db.storage/migration config-map)
+               (:framework.app/emails config-map)
+               (:framework.app/auth config-map))
+              nil))))
+
+;; test if the web-server map is not empty
+(deftest web-server-map-not-empty
+  (let [web-server-map (:framework.app/web-server config-map)]
+    (is (= (empty? web-server-map) false))))
+
+;; test if the web-server map contains the expected keys (:port/:join?)
+(deftest contain-expected-web-server-map-keys
+  (let [web-server-map (:framework.app/web-server config-map)]
+    (is (not (= (and (map? web-server-map)
+                     (:port web-server-map)
+                     (:join? web-server-map))
+                nil)))))
