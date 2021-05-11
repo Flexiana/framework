86046b3da27fbe74d04e2c0efcfd780569fe4393
lambdart <lambdart@protonmail.com>
2021-05-11 18:58:38 -0300

    Simplify session initialization

---
+++ src/framework/session/core.clj
@@ -0,0 +1,30 @@
+(ns framework.session.core
+  (:import (java.util UUID)))
+
+;; define session protocol
+(defprotocol Session
+  ;; fetch an element (no side effect)
+  (fetch [_ k])
+  ;; add an element (side effect)
+  (add! [_ k v])
+  ;; delete an element (side effect)
+  (delete! [_ k])
+  ;; erase all elements (side effect)
+  (erase [_]))
+
+(defn init-in-memory
+  "Initialize session in memory."
+  ([] (init-in-memory (atom {})))
+  ([m]
+   ;; implement the Session protocol
+   (reify Session
+     ;; fetch session key:element
+     (fetch [_ k] (get @m k))
+     ;; add session key:element
+     (add! [_ k v]
+       (let [k (or k (UUID/randomUUID))]
+         (swap! m assoc k v)))
+     ;; delete session key:element
+     (delete! [_ k] (swap! m dissoc k))
+     ;; erase session
+     (erase [_] @m))))
