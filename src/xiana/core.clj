59c1037e45438cf58f2bd279880854c05e9c75f1
lambdart <lambdart@protonmail.com>
2021-05-11 18:46:05 -0300

    Add commentaries and flow macro documentation

--- src/xiana/core.clj
+++ src/xiana/core.clj
@@ -1,22 +1,33 @@
 (ns xiana.core
   (:require
-    [cats.core :as m]
-    [cats.monad.either :as me]))
+   [cats.core :as m]
+   [cats.monad.either :as me]))
 
-(defrecord State
-  [request request-data response session-data deps])
+;; state/context record definition
+(defrecord State [request request-data response session-data deps])
 
+;; monad.either/right container alias
+;; don't stop the sequence of executions, continue! (implicit)
 (def ok me/right)
 
+;; monad.either/left container alias
+;; and stop the sequence of executions (implicit)
+;; used by >>= function
 (def error me/left)
 
+;; monad.extract alias
+;; unwrap monad container
 (def extract m/extract)
 
 (defmacro flow->
+  "Expand a single form to (form) or the sequence of forms to:
+  (lambda (x) (form1 x), (lambda (x) (form2 x)) ...
+  and perform Haskell-style left-associative bind using the
+  monad.either/right context (wrapped)."
   [state & forms]
   `(m/>>=
-     (ok ~state)
-     ~@(for [form forms]
-         (if (seq? form)
-           `(fn [~'x] (~(first form) ~'x ~@(rest form)))
-           form))))
+    (ok ~state)
+    ~@(for [form forms]
+        (if (seq? form)
+          `(fn [~'x] (~(first form) ~'x ~@(rest form)))
+          form))))
