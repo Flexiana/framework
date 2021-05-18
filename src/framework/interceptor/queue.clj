78e2eb5253d50d4fb231e24f02182c2c5a6e1d35
lambdart <lambdart@protonmail.com>
2021-05-18 16:27:24 -0300

    Simplify internal interceptor queue execute function

--- src/framework/interceptor/queue.clj
+++ src/framework/interceptor/queue.clj
@@ -2,54 +2,37 @@
   (:require
    [xiana.core :as xiana]))
 
-(defn- -execute-fn
-  "Execute the interceptor function (enter or leave)."
-  [state interceptor-fn]
-  (let [container-fn (or interceptor-fn xiana/ok)]
-    (container-fn state)))
-
-;; TODO: simplify this function
+;; Question: how to handle the error state from the interceptors?
 (defn- -execute
   "Execute interceptors functions (the enter/leave procedures)
   passing the state as its arguments."
   [state interceptors action]
-  ;; finish the interceptors enter stack? if so, call the action
-  (if (empty? interceptors)
-    (action state)
-    ;; otherwise fetch the enter-fn, leave-fn from the top interceptor
-    (let [{:keys [enter leave error]} (first interceptors)]
-      ;; wrap the interceptor stack execution to our flow (monad/either)
-      (try
-        (xiana/flow-> state
-                      ;; execute enter interceptor function
-                      (-execute-fn enter)
-                      ;; recursive call, implicit: execute all enter-fn first
-                      (-execute (rest interceptors) action)
-                      ;; execute leave interceptors function
-                      (-execute-fn leave))
-        ;; otherwise return the state container with the response
-        ;; wrapped (failure: /monad/either/left)
-        (catch Exception e
-          ;; if we have a error function defined call it
-          (if error
-            (error state)
-            ;; otherwise return the monad/either/right state container
-            (xiana/error
-             (assoc state
-                    :response
-                    {:status 500 :body (Throwable->map e)}))))))))
+  (let [state (xiana/ok state)
+        enter (mapv :enter interceptors)
+        leave (reverse (mapv :leave interceptors))
+        queue (apply concat [enter action leave])]
+    (try
+      ;; execute interceptors/action monad wrapped functions queue
+      (xiana/apply-flow-> state queue)
+      ;; handle exceptions
+      (catch Exception e
+        (xiana/error
+         (assoc state
+                :response
+                {:status 500 :body (Throwable->map e)}))))))
 
 (defn- -concat
   "Concatenate routes interceptors with the defaults ones,
   or override it if its type isn't a map."
   [interceptors default-interceptors]
   (if (map? interceptors)
+    ;; get around/inside interceptors
     (let [around-interceptors (get interceptors :around)
           inside-interceptors (get interceptors :inside)]
       (concat around-interceptors
               default-interceptors
               inside-interceptors))
-    ;; override
+    ;; else override
     (or interceptors default-interceptors)))
 
 (defn execute
@@ -62,4 +45,4 @@
         action (get-in state [:request-data :action])]
     ;; execute the interceptors queue calling the action
     ;; between its enter/leave stacks
-    (-execute state interceptors action)))
+    (-execute state interceptors [action])))
