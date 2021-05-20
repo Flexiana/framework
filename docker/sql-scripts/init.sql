d6ef9818a8f00432da4386380a85165482dc5b0b
lambdart <lambdart@protonmail.com>
2021-05-20 11:48:33 -0300

    Add SQL statements to avoid setup errors

--- docker/sql-scripts/init.sql
+++ docker/sql-scripts/init.sql
@@ -1,5 +1,11 @@
+--;;
+DROP DATABASE IF EXISTS framework;
+
+--;;
 CREATE DATABASE framework;
 
+--;;
 GRANT ALL PRIVILEGES ON DATABASE framework TO postgres;
 
+--;;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
