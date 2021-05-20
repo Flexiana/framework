b0ec05e1798707c87cc0d40b940d71f616623b59
lambdart <lambdart@protonmail.com>
2021-05-20 18:44:51 -0300

    Add parse function to provide interceptors exception handling

--- src/framework/interceptor/queue.clj
+++ src/framework/interceptor/queue.clj
@@ -2,23 +2,30 @@
   (:require
    [xiana.core :as xiana]))
 
-;; Question: how to handle the error state from the interceptors?
+(defn- interceptor->fn
+  "Parse the interceptor function 'side' (:enter/:leave) to
+  a lambda function that uses the try-catch approach to
+  handle the interceptor exception if occurs."
+  [side interceptor]
+  (let [f (side interceptor)]
+    (fn [state]
+      (try
+        (f state)
+        (catch Exception e
+          (let [f (or (:error interceptor) xiana/error)]
+            (f (-> state (assoc :response
+                                {:status 500
+                                 :body   (Throwable->map e)})))))))))
+
 (defn- -execute
   "Execute interceptors functions (the enter/leave procedures)
   passing the state as its arguments."
   [state interceptors action]
-  (let [enter (mapv :enter interceptors)
-        leave (reverse (mapv :leave interceptors))
-        queue (apply concat [enter action leave])]
-    (try
-      ;; execute interceptors/action monad wrapped functions queue
-      (xiana/apply-flow-> state queue)
-      ;; handle exceptions
-      (catch Exception e
-        (xiana/error
-         (assoc state
-                :response
-                {:status 500 :body (Throwable->map e)}))))))
+  (let [enter-fns (mapv #(interceptor->fn :enter %) interceptors)
+        leave-fns (mapv #(interceptor->fn :leave %) interceptors)
+        queue-fns (apply concat [enter-fns action (reverse leave-fns)])]
+    ;; apply flow: execute the queue of interceptors functions
+    (xiana/apply-flow-> state queue-fns)))
 
 (defn- -concat
   "Concatenate routes interceptors with the defaults ones,
