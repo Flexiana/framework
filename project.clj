9457de8af119ae0825e1c97dd55d6baa158ec203
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-09-22 10:58:32 +0200

    Executing router interceptors, v0.2.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.flexiana/framework "0.2.4"
+(defproject com.flexiana/framework "0.2.5"
   :description "Framework"
   :url "https://github.com/Flexiana/framework"
   :license {:name "FIXME" :url "FIXME"}
