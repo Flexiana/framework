305d0bf3bb3297adcf34572541e5224a7d3c976c
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-10-12 16:26:23 +0200

    Thinning acl example

--- examples/acl/src/backend/acl.clj
+++ examples/acl/src/backend/acl.clj
@@ -10,10 +10,8 @@
     [framework.interceptor.core :as interceptors]
     [framework.rbac.core :as rbac]
     [framework.route.core :as routes]
-    [framework.session.core :as session]
     [framework.webserver.core :as ws]
     [interceptors.load-user :as user]
-    [nrepl.server :refer [start-server]]
     [reitit.ring :as ring]))
 
 (def routes
@@ -21,49 +19,44 @@
    ["/re-frame" {:action re-frame/handle-index}]
    ["/assets/*" (ring/create-resource-handler)]
    ["" {:handler ws/handler-fn}
-    ["/posts" {:get    {:action posts-controllers/fetch
+    ["/posts" {:get    {:action     posts-controllers/fetch
                         :permission :posts/read}
-               :put    {:action posts-controllers/add
+               :put    {:action     posts-controllers/add
                         :permission :posts/create}
-               :post   {:action posts-controllers/update-post
+               :post   {:action     posts-controllers/update-post
                         :permission :posts/update}
-               :delete {:action posts-controllers/delete-post
+               :delete {:action     posts-controllers/delete-post
                         :permission :posts/delete}}]
-    ["/posts/ids" {:post {:action posts-controllers/fetch-by-ids
+    ["/posts/ids" {:post {:action     posts-controllers/fetch-by-ids
                           :permission :posts/read}}]
-    ["/posts/comments" {:get {:action posts-controllers/fetch-with-comments
+    ["/posts/comments" {:get {:action     posts-controllers/fetch-with-comments
                               :permission :posts/read}}]
-    ["/comments" {:get    {:action comments-controllers/fetch
+    ["/comments" {:get    {:action     comments-controllers/fetch
                            :permission :comments/read}
-                  :put    {:action comments-controllers/add
+                  :put    {:action     comments-controllers/add
                            :permission :comments/create}
-                  :post   {:action comments-controllers/update-comment
+                  :post   {:action     comments-controllers/update-comment
                            :permission :comments/update}
-                  :delete {:action comments-controllers/delete-comment
+                  :delete {:action     comments-controllers/delete-comment
                            :permission :comments/delete}}]
-    ["/users" {:get    {:action users-controllers/fetch
+    ["/users" {:get    {:action     users-controllers/fetch
                         :permission :users/read}
-               :put    {:action users-controllers/add
+               :put    {:action     users-controllers/add
                         :permission :users/create}
-               :post   {:action users-controllers/update-user
+               :post   {:action     users-controllers/update-user
                         :permission :users/update}
-               :delete {:action users-controllers/delete-user
+               :delete {:action     users-controllers/delete-user
                         :permission :users/delete}}]
-    ["/users/posts" {:get {:action users-controllers/fetch-with-posts
+    ["/users/posts" {:get {:action     users-controllers/fetch-with-posts
                            :permission :users/read}}]
-    ["/users/posts/comments" {:get {:action users-controllers/fetch-with-posts-comments
+    ["/users/posts/comments" {:get {:action     users-controllers/fetch-with-posts-comments
                                     :permission :users/read}}]]])
-
-(defonce server (start-server :port 7888))
 
 (defn system
   [config]
-  (let [session-backend (:session-backend config (session/init-in-memory))
-        deps {:webserver               (:framework.app/web-server config)
+  (let [deps {:webserver               (:framework.app/web-server config)
               :routes                  (routes/reset routes)
               :role-set                (rbac/init (:framework.app/role-set config))
-              :auth                    (:framework.app/auth config)
-              :session-backend         session-backend
               :router-interceptors     []
               :controller-interceptors [(interceptors/muuntaja)
                                         interceptors/params
