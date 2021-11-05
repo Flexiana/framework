e93ad9c2f017a5036f3f1a27707621d40b38e2ae
Ian Fernandez <d.ian.b@live.com>
2021-11-05 16:11:03 -0300

    Get db into development state

--- src/framework/db/core.clj
+++ src/framework/db/core.clj
@@ -1,7 +1,16 @@
 (ns framework.db.core
   "Data source builder"
   (:require
-    [next.jdbc :as jdbc]))
+    [next.jdbc :as jdbc]
+    [honeysql-postgres.format]
+    [honeysql.core :as sql]
+    [xiana.core :as xiana])
+  (:import
+    (javax.sql
+      DataSource)
+    (org.postgresql.jdbc
+      PgConnection)))
+
 
 (defn start
   "Creates datasource.
@@ -11,3 +20,53 @@
   (assoc config
          :datasource
          (jdbc/get-datasource postgresql)))
+
+(defn ->sql-params
+  "Parse sql-map using honeysql format function with pre-defined
+  options that target postgresql."
+  [sql-map]
+  (sql/format sql-map
+              {:quoting            :ansi
+               :parameterizer      :postgresql
+               :return-param-names false}))
+
+(defn execute!
+  "Gets datasource, parse the given sql-map (query) and
+  execute it using `jdbc/execute!`, and returns the modified keys"
+  [datasource sql-map]
+  {:pre [(instance? DataSource datasource)]}
+  (with-open [connection (.getConnection datasource)]
+    (let [sql-params (->sql-params sql-map)]
+      (jdbc/execute! connection sql-params {:return-keys true}))))
+
+(defn in-transaction
+  [tx sql-map]
+  {:pre [(instance? PgConnection tx)]}
+  (let [sql-params (->sql-params sql-map)]
+    (jdbc/execute! tx sql-params {:return-keys true})))
+
+(defn multi-execute!
+  [datasource {:keys [queries transaction?]}]
+  (if transaction?
+    (jdbc/with-transaction [tx datasource]
+                           (mapv #(in-transaction tx %) queries))
+    (mapv #(execute! datasource %) queries)))
+
+(def db-access
+  "Database access interceptor, works from `:query` and from `db-queries` keys
+  Enter: nil.
+  Leave: Fetch and execute a given query using the chosen database
+  driver, if succeeds associate its results into state response data.
+  Remember the entry query must be a sql-map, e.g:
+  {:select [:*] :from [:users]}."
+  {:leave
+   (fn [{query      :query
+         db-queries :db-queries
+         :as        state}]
+     (let [datasource (get-in state [:deps :db :datasource])
+           db-data (cond-> []
+                           query (into (execute! datasource query))
+                           db-queries (into (multi-execute! datasource db-queries))
+                           :always seq)]
+       (xiana/ok
+         (assoc-in state [:response-data :db-data] db-data))))})
