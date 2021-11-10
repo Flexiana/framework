015748d7c8326b1bad937ae631526def501ff89e
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-11-10 11:29:02 +0100

    Update sessions example

--- examples/sessions/src/backend/app/core.clj
+++ examples/sessions/src/backend/app/core.clj
@@ -1,22 +1,49 @@
 (ns app.core
   (:require
-    [app.router :as router]
-    [framework.config.core :as x-config]
+    [app.controllers.index :as index]
+    [app.controllers.login :as login]
+    [app.controllers.logout :as logout]
+    [app.controllers.secret :as secret]
+    [app.interceptors :refer [inject-session?
+                              logout
+                              require-logged-in]]
+    [framework.config.core :as config]
+    [framework.handler.core :as x-handler]
     [framework.interceptor.core :as x-interceptors]
     [framework.route.core :as x-routes]
     [framework.session.core :as x-session]
-    [framework.webserver.core :as x-ws]))
+    [framework.webserver.core :as ws]
+    [piotr-yuxuan.closeable-map :refer [closeable-map]]))
 
-(defn system
-  [config]
-  (let [deps {:webserver               (:framework.app/web-server config)
-              :routes                  (x-routes/reset router/routes)
-              :session-backend         (x-session/init-in-memory)
-              :controller-interceptors [x-interceptors/params
-                                        (x-session/protected-interceptor "" "/login")]}]
-    (x-ws/start deps)))
+(def routes
+  [["" {:handler x-handler/handler-fn}]
+   ["/" {:action       index/index
+         :interceptors [x-interceptors/params
+                        inject-session?]}]
+   ["/login" {:action login/login-controller}]
+   ["/logout" {:action       logout/logout-controller
+               :interceptors {:around [logout]}}]
+   ["/secret" {:action       secret/protected-controller
+               :interceptors {:inside [require-logged-in]}}]])
+
+(defn ->system
+  [app-cfg]
+  (-> (config/config)
+      (merge app-cfg)
+      x-routes/reset
+      x-session/init-in-memory
+      ws/start
+      (select-keys [:routes
+                    :session-backend
+                    :controller-interceptors
+                    :webserver])
+      closeable-map))
+
+(def app-cfg
+  {:routes routes
+   :controller-interceptors [x-interceptors/params
+                             (x-session/protected-interceptor "" "/login")]})
 
 (defn -main
   [& _args]
-  (let [config (x-config/env)]
-    (system config)))
+  (->system app-cfg))
--- examples/sessions/test/integration.clj
+++ examples/sessions/test/integration.clj
@@ -3,8 +3,6 @@
     [app.core :as app]
     [clj-http.client :as http]
     [clojure.test :refer [deftest is use-fixtures]]
-    [framework.config.core :as x-config]
-    [framework.webserver.core :as x-ws]
     [jsonista.core :as json])
   (:import
     (java.util
@@ -12,32 +10,28 @@
 
 (defn std-system-fixture
   [f]
-  (try
-    (-> (x-config/env)
-        app/system)
-    (f)
-    (finally
-      (x-ws/stop))))
+  (with-open [_ (app/->system app/app-cfg)]
+    (f)))
 
 (use-fixtures :once std-system-fixture)
 
 (deftest testing-without-login
   (is (= {:status 200, :body "Index page"}
-         (-> {:url                  "http://localhost:3000/"
+         (-> {:url                  "http://localhost:3333/"
               :unexceptional-status (constantly true)
               :method               :get}
              http/request
              (select-keys [:status :body])))
       "Index page is always available")
   (is (= {:status 401, :body "{\"message\":\"Invalid or missing session\"}"}
-         (-> {:url                  "http://localhost:3000/secret"
+         (-> {:url                  "http://localhost:3333/secret"
               :unexceptional-status (constantly true)
               :method               :get}
              http/request
              (select-keys [:status :body])))
       "Should login to see the secret page")
   (is (= {:status 401, :body "{\"message\":\"Invalid or missing session\"}"}
-         (-> {:url                  "http://localhost:3000/wrong"
+         (-> {:url                  "http://localhost:3333/wrong"
               :unexceptional-status (constantly true)
               :method               :get}
              http/request
@@ -45,7 +39,7 @@
       "A missing page is hidden too"))
 
 (deftest login-fails-on-empty-body
-  (let [login-request {:url                  "http://localhost:3000/login"
+  (let [login-request {:url                  "http://localhost:3333/login"
                        :unexceptional-status (constantly true)
                        :method               :post}
         login-response (http/request login-request)]
@@ -54,7 +48,7 @@
            (select-keys login-response [:status :body])))))
 
 (deftest testing-with-login
-  (let [login-request {:url                  "http://localhost:3000/login"
+  (let [login-request {:url                  "http://localhost:3333/login"
                        :unexceptional-status (constantly true)
                        :body                 (json/write-value-as-string
                                                {:email    "piotr@example.com"
@@ -77,7 +71,7 @@
     (is (= body-session-id header-session-id)
         "Session ID is the same in the response and from headers")
     (is (= {:status 200, :body "Index page, for Piotr"}
-           (-> {:url                  "http://localhost:3000/"
+           (-> {:url                  "http://localhost:3333/"
                 :headers              {:session-id body-session-id}
                 :unexceptional-status (constantly true)
                 :method               :get}
@@ -85,7 +79,7 @@
                (select-keys [:status :body])))
         "Index page has some surprise when user logged in")
     (is (= {:status 200, :body "Hello Piotr"}
-           (-> {:url                  "http://localhost:3000/secret"
+           (-> {:url                  "http://localhost:3333/secret"
                 :unexceptional-status (constantly true)
                 :headers              {:session-id body-session-id}
                 :method               :get}
@@ -94,7 +88,7 @@
         "The secret page is visible with login")
     (is (= {:body   "Not Found"
             :status 404}
-           (-> {:url                  "http://localhost:3000/wrong"
+           (-> {:url                  "http://localhost:3333/wrong"
                 :unexceptional-status (constantly true)
                 :headers              {:session-id body-session-id}
                 :method               :get}
@@ -103,7 +97,7 @@
         "A missing page is missing")))
 
 (deftest testing-with-logout
-  (let [login (-> {:url                  "http://localhost:3000/login"
+  (let [login (-> {:url                  "http://localhost:3333/login"
                    :unexceptional-status (constantly true)
                    :body                 (json/write-value-as-string
                                            {:email    "piotr@example.com"
@@ -122,7 +116,7 @@
                     (catch Exception _ false)))
         "and has UUID session id")
     (is (= {:status 200 :body "Piotr logged out"}
-           (-> {:url                  "http://localhost:3000/logout"
+           (-> {:url                  "http://localhost:3333/logout"
                 :headers              {:session-id session-id}
                 :unexceptional-status (constantly true)
                 :method               :get}
@@ -130,7 +124,7 @@
                (select-keys [:status :body])))
         "User logged out")
     (is (= {:status 200, :body "Index page"}
-           (-> {:url                  "http://localhost:3000/"
+           (-> {:url                  "http://localhost:3333/"
                 :headers              {:session-id session-id}
                 :unexceptional-status (constantly true)
                 :method               :get}
@@ -138,7 +132,7 @@
                (select-keys [:status :body])))
         "Index page is always available")
     (is (= {:status 401, :body "{\"message\":\"Invalid or missing session\"}"}
-           (-> {:url                  "http://localhost:3000/secret"
+           (-> {:url                  "http://localhost:3333/secret"
                 :unexceptional-status (constantly true)
                 :headers              {:session-id session-id}
                 :method               :get}
@@ -146,7 +140,7 @@
                (select-keys [:status :body])))
         "The secret page gets hidden")
     (is (= {:status 401, :body "{\"message\":\"Invalid or missing session\"}"}
-           (-> {:url                  "http://localhost:3000/wrong"
+           (-> {:url                  "http://localhost:3333/wrong"
                 :unexceptional-status (constantly true)
                 :headers              {:session-id session-id}
                 :method               :get}
