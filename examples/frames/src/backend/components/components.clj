3093e48b334bc55f81102a549eada76d77a4cdcf
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-11-10 11:11:32 +0100

    Update frames example

--- examples/frames/src/backend/components/components.clj
+++ examples/frames/src/backend/components/components.clj
@@ -1,18 +1,34 @@
 (ns components
   (:require
-    [app]
+    [controllers.index :as index]
+    [controllers.status :as status]
     [framework.config.core :as config]
     [framework.interceptor.core :as interceptor]
-    [framework.route.core :as routes]
-    [framework.webserver.core :as ws]))
+    [framework.route.core :as router]
+    [framework.webserver.core :as ws]
+    [piotr-yuxuan.closeable-map :refer [closeable-map]]
+    [reitit.ring :as ring]))
 
-(defn deps
-  [config]
-  {:routes (routes/reset app/routes)
-   :webserver (:framework.app/web-server config)
+(def routes
+  [["/" {:get {:action index/handle-index}}]
+   ["/status" {:get {:action status/handle-status}}]
+   ["/assets/*" (ring/create-resource-handler {:path "/"})]])
+
+(defn ->system
+  [app-cfg]
+  (-> (config/config)
+      (merge app-cfg)
+      router/reset
+      ws/start
+      (select-keys [:routes
+                    :webserver
+                    :controller-interceptors])
+      closeable-map))
+
+(def app-cfg
+  {:routes                  routes
    :controller-interceptors [(interceptor/muuntaja)]})
 
 (defn -main
   [& args]
-  (let [config (config/env)]
-    (ws/start (deps config))))
+  (->system app-cfg))
