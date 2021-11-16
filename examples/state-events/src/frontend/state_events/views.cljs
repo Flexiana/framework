b167c5328275e2023b19847c39523e3a8b57ee01
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-11-16 15:08:02 +0100

    basic view

--- examples/state-events/src/frontend/state_events/views.cljs
+++ examples/state-events/src/frontend/state_events/views.cljs
@@ -1,26 +1,40 @@
 (ns state-events.views
   (:require
-   [re-frame.core :as re-frame]
-   [state-events.subs :as subs]))
+    [re-frame.core :as re-frame]
+    [state-events.subs :as subs]
+    [clojure.string :as str]))
 
+(defonce st (reagent.core/atom {}))
+
+(defn input-gr [atm k]
+  [:div.input-group
+   [:span.input-group-text (apply str (str/upper-case (first (name k))) (rest (name k)))]
+   [:input.form-control {:type "text" :aria-label (name k) :on-change #(swap! st assoc k (-> % .-target .-value))}]
+   [:button {:type "button" :label "Last name"
+             :on-click #(swap! atm merge {k (get @st k)})} "Add"]])
+
+(defn info-panel [atm]
+  [:div {:style {:margin-left 50}}
+   [:table {:class :table-bordered
+            :style {:min-width 350}}
+    [:thead [:tr [:th "Key"] [:th "Value"]]]
+    [:tbody (map (fn [[k v]] [:tr [:td k] [:td v]]) @atm)]]
+   [:button {:on-click #(reset! atm {})} "Undo"]])
 
 (defn main-panel [atm]
-  (let [name (re-frame/subscribe [::subs/name])]
-    [:div {:style {:width "500px"
-                   :margin :auto}}
-     [:label "Add user"]
-     [:p]
-     [:form
-      [:div.mb-3
-       [:label.form-label {:for "exampleInputEmail1"} "Email address"]
-       [:input#exampleInputEmail1.form-control {:type "email" :aria-describedby "emailHelp"}]
-       [:div#emailHelp.form-text "We'll never share your email with anyone else."]]
-      [:div.mb-3
-       [:label.form-label {:for "exampleInputPassword1"} "Password"]
-       [:input#exampleInputPassword1.form-control {:type "password"}]]
-      [:div.mb-3.form-check
-       [:input#exampleCheck1.form-check-input {:type "checkbox"}]
-       [:label.form-check-label {:for "exampleCheck1"} "Check me out"]]
-      [:button.btn.btn-primary {:type "submit"} "Submit"]]]))
+  [:div
+   [:div {:class "d-flex flex-row bd-highlight mb-3"
+          :style {:margin-left 100
+                  :margin-top  100}}
+    [:div
+     (input-gr atm :first-name)
+     (input-gr atm :last-name)
+     (input-gr atm :e-mail)
+     (input-gr atm :phone)
+     (input-gr atm :city)]
+    (info-panel atm)]])
+
+
+
 
 
