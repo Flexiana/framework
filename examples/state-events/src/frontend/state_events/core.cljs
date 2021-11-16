db2f74f0c4cc339818e3d3cbc48a52f0f092666b
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-11-16 12:42:54 +0100

    WIP

--- examples/state-events/src/frontend/state_events/core.cljs
+++ examples/state-events/src/frontend/state_events/core.cljs
@@ -6,6 +6,7 @@
    [state-events.views :as views]
    [state-events.config :as config]))
 
+(defonce atm (reagent.core/atom {}))
 
 (defn dev-setup []
   (when config/debug?
@@ -15,7 +16,7 @@
   (re-frame/clear-subscription-cache!)
   (let [root-el (.getElementById js/document "app")]
     (rdom/unmount-component-at-node root-el)
-    (rdom/render [views/main-panel] root-el)))
+    (rdom/render [views/main-panel atm] root-el)))
 
 (defn init []
   (re-frame/dispatch-sync [::events/initialize-db])
--- examples/state-events/src/frontend/state_events/views.cljs
+++ examples/state-events/src/frontend/state_events/views.cljs
@@ -1,11 +1,26 @@
 (ns state-events.views
   (:require
    [re-frame.core :as re-frame]
-   [state-events.subs :as subs]
-   ))
+   [state-events.subs :as subs]))
 
-(defn main-panel []
+
+(defn main-panel [atm]
   (let [name (re-frame/subscribe [::subs/name])]
-    [:div
-     [:h1 "Hello from " @name]
-     ]))
+    [:div {:style {:width "500px"
+                   :margin :auto}}
+     [:label "Add user"]
+     [:p]
+     [:form
+      [:div.mb-3
+       [:label.form-label {:for "exampleInputEmail1"} "Email address"]
+       [:input#exampleInputEmail1.form-control {:type "email" :aria-describedby "emailHelp"}]
+       [:div#emailHelp.form-text "We'll never share your email with anyone else."]]
+      [:div.mb-3
+       [:label.form-label {:for "exampleInputPassword1"} "Password"]
+       [:input#exampleInputPassword1.form-control {:type "password"}]]
+      [:div.mb-3.form-check
+       [:input#exampleCheck1.form-check-input {:type "checkbox"}]
+       [:label.form-check-label {:for "exampleCheck1"} "Check me out"]]
+      [:button.btn.btn-primary {:type "submit"} "Submit"]]]))
+
+
