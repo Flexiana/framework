4fc64c019629ae3ee9c220998316bc635393f3c9
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2021-11-29 14:24:50 +0100

    Fixing SSE and cookies bugs

--- src/framework/cookies/core.clj
+++ src/framework/cookies/core.clj
@@ -5,20 +5,24 @@
     [ring.middleware.cookies :as cookies]
     [xiana.core :as xiana]))
 
-(defn- parse-request-cookies
-  [req]
-  (keywordize-keys
-    (cookies/cookies-request req)))
-
-(defn- parse-response-cookies
-  [resp]
-  (cookies/cookies-response resp))
-
 (def interceptor
   "Parses request and response cookies"
-  {:enter (fn [state]
-            (xiana/ok
-              (update state :request
-                      parse-request-cookies)))
-   :exit  (fn [state]
-            (xiana/ok (update state :response parse-response-cookies)))})
+  (letfn [(move-cookies
+            [req]
+            (if (get-in req [:headers "cookie"])
+              req
+              (assoc-in req [:headers "cookie"]
+                        (get-in req [:headers :cookie]))))
+          (parse-request-cookies
+            [req]
+            (keywordize-keys
+              (cookies/cookies-request (move-cookies req))))
+          (parse-response-cookies
+            [resp]
+            (cookies/cookies-response resp))]
+    {:enter (fn [state]
+              (xiana/ok
+                (update state :request
+                        parse-request-cookies)))
+     :leave (fn [state]
+              (xiana/ok (update state :response parse-response-cookies)))}))
