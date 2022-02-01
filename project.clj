559db1772e48e1eaa9fdd1f12a0b37751fb70747
Stas Makarov <stas.makarov@flexiana.com>
2022-02-01 13:12:35 +0100

    update yougthous config

--- project.clj
+++ project.clj
@@ -29,7 +29,7 @@
                  [piotr-yuxuan/closeable-map "0.35.0"]
                  [potemkin "0.4.5"]
                  [seancorfield/next.jdbc "1.2.659"]
-                 [yogthos/config "1.1.7"]]
+                 [yogthos/config "1.1.9"]]
   :source-paths ["src"]
   :target "target/%s/"
   :profiles {:dev      {:resource-paths         ["config/dev"]}
