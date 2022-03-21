aa91808097aa16b27d1a7cafa93c96958c970e78
Stas Makarov <stas.makarov@flexiana.com>
2022-03-21 20:13:34 +0400

    remove main in seed

--- src/framework/db/seed.clj
+++ src/framework/db/seed.clj
@@ -1,6 +1,5 @@
 (ns framework.db.seed
   (:require
-    [framework.config.core :as config]
     [migratus.core :as migratus]
     [next.jdbc :as jdbc]
     [xiana.commons :refer [rename-key]]))
@@ -16,22 +15,3 @@
                         (rename-key :seeds-table-name :migration-table-name))]
      (migratus/migrate mig-config))
    config))
-
-(defn seed-config []
-  (let [config  (config/config)
-        db-conf (:xiana/postgresql config)]
-    (-> (assoc (:xiana/migration config) :db db-conf)
-        (rename-key :seeds-dir :migration-dir)
-        (rename-key :seeds-table-name :migration-table-name))))
-
-(defn -main [& args]
-  (let [[command name type] args
-        seed-config         (seed-config)]
-    (if (and (:migration-dir seed-config) (:migration-table-name seed-config))
-      (case command
-        "create"  (migratus/create seed-config name (keyword type))
-        "reset"   (migratus/reset seed-config)
-        "destroy" (migratus/destroy seed-config)
-        "migrate" (migratus/migrate seed-config)
-        (println "You can 'create' 'reset' 'destroy' or 'migrate' your seed data"))
-      (println "No seed configuration found"))))
