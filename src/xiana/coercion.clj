538861502f9be4a964c8caf1bb47c7313c4f081b
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2022-04-13 11:59:19 +0200

    fix coercion interceptor

--- src/xiana/coercion.clj
+++ src/xiana/coercion.clj
@@ -24,7 +24,10 @@
     [malli.core :as m]
     [malli.registry :as mr]
     [malli.util :as mu]
-    [reitit.coercion :as coercion]))
+    [reitit.coercion :as coercion]
+    [malli.error :as me]
+    [taoensso.timbre :as log])
+  (:import (clojure.lang ExceptionInfo)))
 
 (defn registry
   "Registers a given schema in malli"
@@ -45,7 +48,7 @@
                               coercion/coerce!)]
                    (update-in state [:request :params] merge cc))
                  (catch Exception _
-                   (throw (ex-info "Request coercion failed" {:status 400 :body   "Request coercion failed"})))))
+                   (throw (ex-info "Request coercion failed" {:status 400 :body "Request coercion failed"})))))
    :leave (fn [{{:keys [:status :body]} :response
                 :as                     state}]
             (let [schema (get-in state [:request-data :match :data :responses status :body])]
@@ -54,3 +57,35 @@
                                                       {:status 400
                                                        :body   "Response validation failed"}))
                     :else state)))})
+
+(def interceptor
+  "On enter: validates request parameters
+  On leave: validates response body
+  on request error: responds {:status 400, :body \"Request coercion failed\"}
+  on response error: responds {:status 400, :body \"Response validation failed\"}"
+  {:enter (fn [state]
+            (try (let [cc (-> (get-in state [:request-data :match])
+                              coercion/coerce!)]
+                   (update-in state [:request :params] merge cc))
+                 (catch ExceptionInfo ex
+                   (throw (ex-info "Request coercion failed" {:status 400
+                                                              :body   {:errors
+                                                                       (mapv (fn [e]
+                                                                               (format "%s should satisfy %s"
+                                                                                       (:in e)
+                                                                                       (m/form (:schema e))))
+                                                                             (:errors (ex-data ex)))}})))))
+   :leave (fn [{{:keys [:status :body]}  :response
+                {method :request-method} :request
+                :as                      state}]
+            (let [schema (or (get-in state [:request-data :match :data method :responses status :body])
+                             (get-in state [:request-data :match :data :responses status :body]))
+                  valid? (some-> schema (m/validate body))]
+              (if (false? valid?)
+                (let [explain (m/explain schema body)
+                      humanized (me/humanize explain)]
+                  (log/error "Response validation failed with " humanized ", response: " body)
+                  (throw (ex-info "Response coercion failed"
+                                  {:status 500
+                                   :body   "Response coercion failed"})))
+                state)))})
