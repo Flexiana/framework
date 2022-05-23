b0f0e01178303d935bbb5d4308277f989665e0dc
Stas Makarov <stas.makarov@flexiana.com>
2022-05-23 19:08:32 +0400

    fix test

--- examples/state-events/project.clj
+++ examples/state-events/project.clj
@@ -1,20 +1,19 @@
 (defproject state-events "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojurescript "1.11.4"]
-                 [com.flexiana/framework "0.5.0-rc1"]
+  :dependencies [[com.flexiana/framework "0.5.0-rc1"]
                  [thheller/shadow-cljs "2.19.0"]
                  [re-frame "1.2.0"]
                  [cljs-ajax "0.8.4"]
-                 [org.clojure/tools.namespace "1.2.0"]
                  [tick "0.5.0-RC5"]]
   :plugins [[lein-shadow "0.4.0"]]
   :main ^:skip-aot state-events.core
   :uberjar-name "state-events.jar"
-  :source-paths ["src/backend" "src/frontend" "src/shared"]
+  :source-paths ["src/backend" #_"src/frontend" "src/shared"]
   :clean-targets ^{:protect false} ["resources/public/assets/js/compiled" "target"]
   :profiles {:dev   {:resource-paths ["dev" "config/dev"]
-                     :dependencies   [[binaryage/devtools "1.0.5"]]}
+                     :dependencies   [[binaryage/devtools "1.0.6"]
+                                      [org.clojure/tools.namespace "1.3.0"]]}
              :local {:resource-paths ["config/local"]}
              :prod  {:resource-paths ["config/prod"]}
              :test  {:resource-paths ["config/test"]
