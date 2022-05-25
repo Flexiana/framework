35b64135ffe494efb0d4b0516cbe767163af682b
Stas Makarov <stas.makarov@flexiana.com>
2022-05-25 17:47:17 +0400

    "fix examples"

--- examples/sessions/project.clj
+++ examples/sessions/project.clj
@@ -7,10 +7,13 @@
             [migratus-lein "0.7.3"]]
   :main ^:skip-aot app.core
   :uberjar-name "frames.jar"
-  :source-paths ["src/backend/" "src/frontend"]
+  :source-paths ["src/backend/"]
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
   :profiles {:dev   {:resource-paths ["config/dev"]
                      :dependencies   [[binaryage/devtools "1.0.5"]]}
+             :frontend {:source-paths ["src/frontend"]
+                        :dependencies [[thheller/shadow-cljs "2.19.0"]
+                                       [re-frame "1.2.0"]]}
              :local {:resource-paths ["config/local"]}
              :prod  {:resource-paths ["config/prod"]}
              :test  {:resource-paths ["config/test"]
@@ -18,7 +21,6 @@
                                       [mvxcvi/cljstyle "0.15.0"
                                        :exclusions [org.clojure/clojure]]]}}
   :shadow-cljs {:nrepl {:port 8777}
-
                 :builds {:app {:target     :browser
                                :output-dir "resources/public/js/compiled"
                                :asset-path "/js/compiled"
@@ -28,9 +30,9 @@
             "ci"      ["do" "clean," "cloverage," "lint," "uberjar"]
             "kondo"   ["run" "-m" "clj-kondo.main" "--lint" "src" "test"]
             "lint"    ["do" "kondo," "eastwood," "kibit"]
-            "watch"   ["with-profile" "dev" "do"
+            "watch"   ["with-profile" "dev,frontend" "do"
                        ["shadow" "watch" "app" "browser-test" "karma-test"]]
-            "release" ["with-profile" "prod" "do"
+            "release" ["with-profile" "prod,frontend" "do"
                        ["shadow" "release" "app"]]}
   :migratus {:store         :database
              :migration-dir "migrations"
