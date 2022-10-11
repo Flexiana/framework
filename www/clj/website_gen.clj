d9d50a81a65048fc76b12dcf370f27e6ec6278eb
Krisztian Gulyas <krisztian.gulyas@flexiana.com>
2022-10-11 18:09:34 +0200

    Fix md-anchors

--- www/clj/website_gen.clj
+++ www/clj/website_gen.clj
@@ -31,16 +31,22 @@
   (second (re-find #"\<h1.*>(.*)\<\/h1>" html)))
 
 (defn read-file [^File f]
-  (let [content (slurp f)
-        html    (md/md-to-html-string content :heading-anchors true)
-        corrected (str/replace html #"<li><a href='#([a-z\-]+)"
-                               (fn [[x _]]
-                                 (str/replace x #"-" "_")))
-        header  (header html)]
+  (let [content   (slurp f)
+        html      (md/md-to-html-string content :heading-anchors true)
+        corrected (-> html
+                      (str/replace #"<li><a href='#([a-z\-]+)"
+                                   (fn [[x _]]
+                                     (str/replace x #"-" "_")))
+                      (str/replace #"<a href='[a-zA-z\-]+\.md"
+                                   #(str/replace-first %1 #"\.md" ".html"))
+                      (str/replace #"<a href='[a-zA-Z\-]+\.html#([a-zA-Z\-]+)'>"
+                                   (fn [[s t]]
+                                     (str/replace s (re-pattern t) (str/replace t #"\-" "_")))))
+        header    (header html)]
     {:source-name (.getName f)
      :markdown    content
      :html        corrected
-     :out-name    (str (str/replace header #"[^a-zA-Z]" "-") ".html")
+     :out-name    (str/lower-case (str/replace (.getName f) #"\.md" ".html"))
      :header      header}))
 
 (defn read-files [files]
@@ -79,7 +85,7 @@
                "doc/how-to.md"
                "doc/contribution.md"
                "doc/getting-started.md"])
-  (read-files [(File. "doc/tutorials.md")])
+  (:html (first (read-files [(File. "doc/tutorials.md")])))
   (-main "--source-dir" "doc" "--target" "www/target" "--sources" "doc/welcome.md"
          "doc/conventions.md"
          "doc/tutorials.md"
