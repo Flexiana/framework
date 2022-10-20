79631b6f5bdeb72757d8445d351ab2a48ba0d211
Seçkin KÜKRER <seckin.kukrer@hotmail.com>
2022-10-20 18:42:27 +0300

    Update and re-factor Swagger implementation.

--- src/xiana/swagger.clj
+++ src/xiana/swagger.clj
@@ -12,11 +12,25 @@
          (= type :edn) identity
          :else identity))))
 
+(defn swagger-configs-there? [config]
+  "Checks if the config has the required keys for swagger functionality.
+   Required keys:
+   * :xiana/swagger
+   * :xiana/swagger-ui"
+  (->> config
+       ((juxt :xiana/swagger-ui :xiana/swagger))
+       (reduce #(and % %2) true)
+       boolean))
+
 (defn ->swagger-data
-  "Update routes."
-  [config]
-  (let [routes-swagger-data (-> config
-                                :routes
-                                .routes
-                                (routes->swagger-json :type :json))]
-    (assoc config :swagger-data routes-swagger-data)))
+  "Update routes for swagger data generation."
+  [config & {routes :routes}]
+  (if (swagger-configs-there? config)
+    (let [routes (or routes (-> config :routes))
+         routes-swagger-data (routes->swagger-json routes
+                                                   :type :json)
+         config-key (-> config
+                        (get :xiana/swagger {})
+                        (get :path :swagger.json))]
+     (assoc config config-key routes-swagger-data))
+    config))
