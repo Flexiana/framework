71cd251e07db365ff86ed6285e0243addeb8d339
Giga Chokheli <giga@chokhe.li>
2023-07-31 16:58:11 +0200

    Error interceptor wrapped in `bad request` response instead of 200

--- src/xiana/interceptor/error.clj
+++ src/xiana/interceptor/error.clj
@@ -1,12 +1,13 @@
-(ns xiana.interceptor.error)
+(ns xiana.interceptor.error
+  (:require [ring.util.response :as ring]))
 
 (def response
   "Handles the exception if there's `ex-info` exception with non-empty
   `:xiana/response` key."
-  {:name ::response
+  {:name  ::error-response
    :error (fn [state]
             (if-let [resp (-> state :error ex-data :xiana/response)]
               (-> state
-                  (assoc :response resp)
+                  (assoc :response (ring/bad-request resp))
                   (dissoc :error))
               state))})
