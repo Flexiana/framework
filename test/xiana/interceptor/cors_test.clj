ee195863db94f04a47858c70ebc9bdc32949fbdf
Giga Chokheli <giga@chokhe.li>
2023-07-31 16:47:11 +0200

    CORS headers interceptor
    - The allowed origin is configurable from env variables and in state it looks for `:cors-url` under `:deps` key
    - It also supports `preflight` requests

---
+++ test/xiana/interceptor/cors_test.clj
@@ -0,0 +1,28 @@
+(ns xiana.interceptor.cors-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [xiana.interceptor.cors :refer [cors-headers interceptor]]))
+
+(deftest cors-interceptor-test
+  (testing "cross-origin-headers interceptor"
+    (let [leave (:leave interceptor)
+          origin "http://localhost:3001"]
+
+      (testing "when request is a preflight request"
+        (let [state {:request {:request-method :options}
+                     :deps    {:cors-origin origin}}
+              expected (update-in state [:response]
+                                  merge
+                                  {:status  200
+                                   :headers (cors-headers origin)
+                                   :body    "preflight complete"})
+              result (leave state)]
+          (is (= expected result))))
+
+      (testing "when request is not a preflight request"
+        (let [state {:request  {:request-method :get}
+                     :deps     {:cors-origin origin}
+                     :response {:headers {}}}
+              expected (update-in state [:response :headers]
+                                  merge (cors-headers origin))
+              result (leave state)]
+          (is (= expected result)))))))
