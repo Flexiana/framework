a22adb7e670e2d48a9899a26b5c87f00b0bf146f
Gustavo Valente <gustavomsvalente@gmail.com>
2023-11-21 02:54:10 -0300

    Fix kebab-camel-interceptor to all request type

--- src/xiana/interceptor/kebab_camel.clj
+++ src/xiana/interceptor/kebab_camel.clj
@@ -4,17 +4,27 @@
     [camel-snake-kebab.extras :as cske]
     [clojure.core.memoize :as mem]))
 
+(def request-type-params [:params :body-params :query-params :path-params :form-params :multipart-params])
+
+(def camel-to-kebab
+  (fn [resp]
+    (cske/transform-keys
+      (mem/fifo csk/->kebab-case {} :fifo/threshold 512) resp)))
+
+(def kebab-to-camel
+  (fn [resp]
+    (cske/transform-keys
+      (mem/fifo csk/->camelCase {} :fifo/threshold 512) resp)))
+
 (def interceptor
   "The purpose is to make Js request compatible with clojure, and response compatible with Javascript.
   :request - {:params { "
   {:name  ::camel-to-kebab-case
    :enter (fn [state]
-            (update-in state [:request :params]
-                       (fn [resp]
-                         (cske/transform-keys
-                           (mem/fifo csk/->kebab-case {} :fifo/threshold 512) resp))))
+            (reduce
+              (fn [state type-param]
+                (update-in state [:request type-param] camel-to-kebab))
+              state
+              request-type-params))
    :leave (fn [state]
-            (update-in state [:response :body]
-                       (fn [resp]
-                         (cske/transform-keys
-                           (mem/fifo csk/->camelCase {} :fifo/threshold 512) resp))))})
+            (update-in state [:response :body] kebab-to-camel))})
