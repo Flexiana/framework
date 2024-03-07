ab1bbd97f482b2d85eccf4c149240049dd09c62c
GV <95759575+gmsvalente@users.noreply.github.com>
2024-03-07 09:02:29 -0300

    Fix set-level to pure function (#285)
    * Fix set-level to pure function
    
    Co-authored-by: Iain Wood <iain.wood@flexiana.com>
    
    * Apply cljstyle and clj-kondo
    
    Co-authored-by: Iain Wood <iain.wood@flexiana.com>
    
    ---------
    
    Co-authored-by: Iain Wood <iain.wood@flexiana.com>

--- src/xiana/logging.clj
+++ src/xiana/logging.clj
@@ -1,10 +1,9 @@
 (ns xiana.logging
   (:require
-    [taoensso.timbre :as log]
-    [xiana.config :as config]))
+    [taoensso.timbre :as log]))
 
 (defn set-level
   [cfg]
-  (when-let [level (-> (config/config) :logging/timbre-config :min-level)]
+  (when-let [level (-> cfg :logging/timbre-config :min-level)]
     (log/set-min-level! level))
   cfg)
